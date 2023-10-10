04e0b69f56aa6d8db2914adbf0b04570ed297b81
Jayanta Barman <jbleelija@gmail.com>
2023-10-10 18:31:25 +0530

    sidebar pages option move on by jayanta

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -118,18 +118,18 @@
         <!-- Nav Item - Utilities Collapse Menu -->
 
         <!-- Divider -->
-        <hr class="sidebar-divider">
+        <!-- <hr class="sidebar-divider"> -->
 
         <!-- Heading -->
-        <div class="sidebar-heading">
+        <!-- <div class="sidebar-heading">
             Addons
-        </div>
+        </div> -->
 
 
 
         <!-- Nav Item - Pages Collapse Menu -->
 
-        <li class="nav-item">
+        <!-- <li class="nav-item">
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
                 aria-expanded="true" aria-controls="collapsePages">
                 <i class="fas fa-fw fa-folder"></i>
@@ -149,7 +149,7 @@
                 </div>
             </div>
 
-        </li>
+        </li> -->
 
 
 
