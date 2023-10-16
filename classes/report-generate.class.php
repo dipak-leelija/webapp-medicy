1b08a0454f2d3f84eeabeaffe4e768ea1bdc98d6
Jayanta Barman <jbleelija@gmail.com>
2023-10-16 17:10:14 +0530

    working on LabReport generate page and lab_report, lab_report_details table

--- classes/report-generate.class.php
+++ classes/report-generate.class.php
@@ -1,76 +1,77 @@
 <?php
 require_once 'dbconnect.php';
-class LabReport extends DatabaseConnection{
+class LabReport extends DatabaseConnection
+{
 
-    function patientDatafetch($patientId){
-        try{
-            $data=0;
+    function patientDatafetch($patientId)
+    {
+        try {
+            $data = 0;
             $sql = "SELECT * FROM `patient_details` where `patient_id` = '$patientId'";
             $query = $this->conn->query($sql);
-            while($result = $query->fetch_object()){
+            while ($result = $query->fetch_object()) {
                 $data = $result;
             }
             $dataset = json_encode($data);
             return $dataset;
-
-        }catch(Exception $e){
+        } catch (Exception $e) {
             echo $e->getMessage();
         }
     }
 
-    function patientTest($testId){
-        try{
+    function patientTest($testId)
+    {
+        try {
 
             $sql = "SELECT * FROM `sub_tests` where `id` = '$testId'";
             $query = $this->conn->query($sql);
-            while($result = $query->fetch_object()){
+            while ($result = $query->fetch_object()) {
                 $data = $result;
             }
             $dataset = json_encode($data);
             return $dataset;
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
 
 
+    function labReportAdd($billId,$patientId,$dateTime,$adminId){
+        try{
+            $sql = "INSERT INTO `lab_report` (`bill_id`,`patient_id`,`added_on`,`admin_id`) VALUES ('$billId','$patientId','$dateTime','$adminId')";
+            $query = $this->conn->query($sql);
+            $labreportId = $this->conn->insert_id;
+            // $insertedId = $this->conn->lastInsertId(); // Retrieve the inserted ID
+            return ["result" => true,"insert_id" =>  $labreportId];
         }catch(Exception $e){
             echo $e->getMessage();
         }
     }
 
-    // function labReportAdd($testValue,$unitValue){
-    //     $testValue = $testValue.$unitValue;
-    //     try{
-    //             $sql = " INSERT INTO `lab_report_detail` (`test_value`) VALUES ('$testValue')";
-    //             $query = $this->conn->query($sql);
-    //             return $sql;
-            
-    //     }catch(Exception $e){
-    //         echo $e->getMessage();
-    //         // return false;
-    //     }
-    // }
 
-    function labReportAdd($testValue, $unitValue,$testId) {
-    try {
-        $testValue = $testValue.'-' . $unitValue;
+    function labReportDetailsAdd($testValue, $unitValue, $testId,$reportId)
+    {
+        try {
+            $testValue = $testValue . ' - ' . $unitValue;
+            $stmt = $this->conn->prepare("INSERT INTO `lab_report_detail` (`report_id`,`test_value`,`test_id`) VALUES (?,?,?)");
 
-        $stmt = $this->conn->prepare("INSERT INTO `lab_report_detail` (`test_value`,`test_id`) VALUES (?,?)");
+            if ($stmt) {
+                $stmt->bind_param("iss", $reportId,$testValue, $testId);
 
-        if ($stmt) {
-            $stmt->bind_param("ss", $testValue,$testId);
-
-            if ($stmt->execute()) {
-                $stmt->close();
-                return true; // Indicates successful insertion
+                if ($stmt->execute()) {
+                    $stmt->close();
+                    return $stmt; // Indicates successful insertion
+                } else {
+                    throw new Exception("Error executing statement: " . $stmt->error);
+                }
             } else {
-                throw new Exception("Error executing statement: " . $stmt->error);
+                throw new Exception("Error preparing statement: " . $this->conn->error);
             }
-        } else {
-            throw new Exception("Error preparing statement: " . $this->conn->error);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            return false; // Indicates failed insertion
         }
-
-    } catch(Exception $e) {
-        echo $e->getMessage();
-        return false; // Indicates failed insertion
-    }
     }
 
+    
 }
