e96f2f79942f2af08e9c72ed07c63e9a43a16200
ROODRO <roodro.leelija@gmail.com>
2023-10-25 18:24:52 +0530

    create a card for showing most purchased distributor by items.

---
+++ admin/components/mopdByItems.php
@@ -0,0 +1,39 @@
+<?php
+require_once dirname(dirname(__DIR__)) . '/config/constant.php';
+
+$includePath = get_include_path();
+
+$maxPurchase = $StockIn->selectDistOnMaxPurchase($adminId);
+$maxPurchase = json_decode($maxPurchase);
+$distNameOnMaxPurchase = $Distributor->distributorDetail($maxPurchase->distributor_id);
+
+// echo $maxPurchase->total_purchase_amount;
+// echo $distNameOnMaxPurchase->name;
+
+$maxItemPurchase = $StockIn->selectDistOnMaxItems($adminId);
+$maxItemPurchase = json_decode($maxItemPurchase);
+
+$distNameOnMaxItem = $Distributor->distributorDetail($maxItemPurchase->distributor_id);
+
+// echo $maxItemPurchase->number_of_purchases;
+// echo $distNameOnMaxItem->name;
+
+
+
+
+?>
+
+<div class="card border-left-info border-right-info h-100 py-2 pending_border animated--grow-in">
+    <div class="card-body pb-0">
+        <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                    most purchaed distributor by item</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">
+                    <label type="text" id="itemCount" name="itemCount"><?php echo $maxItemPurchase->number_of_purchases; ?> Times</label><br>
+                    <label type="text" id="distName" name="distName"><?php echo $distNameOnMaxItem->name;; ?></label>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
