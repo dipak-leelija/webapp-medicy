0ec709213cfc218f12b9ff8e0f40d587c3009c50
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-11-08 17:16:41 +0530

    ajax pages are updated with constant file path

--- ajax/return-distributor-bill-list.ajax.php
+++ ajax/return-distributor-bill-list.ajax.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once dirname(dirname(__DIR__)).'/config/constant.php';
-require_once ADM_DIR.'_config/sessionCheck.php'; //check admin loggedin or not
+require_once dirname(__DIR__).'/config/constant.php';
+require_once ROOT_DIR.'_config/sessionCheck.php'; //check admin loggedin or not
 require_once CLASS_DIR.'dbconnect.php';
 
 require_once CLASS_DIR.'stockIn.class.php';
