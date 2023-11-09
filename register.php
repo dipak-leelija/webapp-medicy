4fa783b310a6b2af411048e84bb4c268c74ff679
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-11-09 16:29:30 +0530

    register page's paths fixed

--- register.php
+++ register.php
@@ -20,13 +20,13 @@ $diffrentPassword = false;
 
 
 if (isset($_POST['register'])) {
-    $Fname = $_POST['fname'];
-    $Lname = $_POST['lname'];
-    $username = $_POST['user-name'];
-    $email = $_POST['email'];
-    $mobNo = $_POST['mobile-number'];
-    $password =  $_POST['password'];
-    $cpassword = $_POST['cpassword'];
+    $Fname      = $_POST['fname'];
+    $Lname      = $_POST['lname'];
+    $username   = $_POST['user-name'];
+    $email      = $_POST['email'];
+    $mobNo      = $_POST['mobile-number'];
+    $password   =  $_POST['password'];
+    $cpassword  = $_POST['cpassword'];
 
     $adminStr1 = 'ADM'.mt_rand(0, 9999999);
     $adminSubStr1 = substr($email, 0, 4);
@@ -89,13 +89,12 @@ if (isset($_POST['register'])) {
     <title>Medicy Health Care - Admin Registration</title>
 
     <!-- Custom fonts for this template-->
-    <link href="admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    <link href="<?= PLUGIN_PATH ?>fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="admin/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?= CSS_PATH ?>sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -197,14 +196,14 @@ if (isset($_POST['register'])) {
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="admin/vendor/jquery/jquery.min.js"></script>
-    <script src="js/bootstrap-js-4/bootstrap.bundle.min.js"></script>
+    <script src="<?= PLUGIN_PATH ?>jquery/jquery.min.js"></script>
+    <script src="<?= JS_PATH ?>bootstrap-js-4/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="admin/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?= PLUGIN_PATH ?>jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="admin/js/sb-admin-2.min.js"></script>
+    <script src="<?= JS_PATH ?>sb-admin-2.min.js"></script>
 
 </body>
 
