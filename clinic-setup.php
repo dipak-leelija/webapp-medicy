3cbc5e8701faf1e1a4ab7f73f3d13a1df506128a
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-11-10 11:50:30 +0530

    conflection fixed

--- clinic-setup.php
+++ clinic-setup.php
@@ -1,22 +1,22 @@
 <?php
-require_once dirname(__DIR__) . '/config/constant.php';
-require_once ADM_DIR . '_config/sessionCheck.php'; //check admin loggedin or not
+$page = "helth-care";
+require_once 'config/constant.php';
+require_once ROOT_DIR.'_config/sessionCheck.php'; //check admin loggedin or not
 
-require_once CLASS_DIR . 'dbconnect.php';
+require_once CLASS_DIR.'dbconnect.php';
 require_once CLASS_DIR.'hospital.class.php';
 
-$page = "helth-care";
 
 //Health Care Class Initilized
-$helthCare = new HelthCare();
+$HealthCare = new HelthCare();
 
 
 // Healthcare Addesss and details
 if (isset($_POST['update']) ==  true) {
 
     $logo = $_FILES['site-logo']['name'];
-    $tempImgname = $_FILES['site-logo']['tmp_name'];
-    $imgFolder = "img/".$logo;
+    $tempImgname    = $_FILES['site-logo']['tmp_name'];
+    $imgFolder      = "images/orgs/".$logo;
     move_uploaded_file($tempImgname, $imgFolder);
 
     $healthCareName          = $_POST['helthcare-name'];
@@ -30,7 +30,7 @@ if (isset($_POST['update']) ==  true) {
     $healthCareHelpLineNo    = $_POST['helpline-no'];
     $healthCareApntBookingNo = $_POST['apnt-booking-no'];
 
-    $UpdateHealthcare = $helthCare->updateHealthCare($imgFolder, $healthCareName, $healthCareAddress1, $healthCareAddress2, $healthCareCity, $healthCareDist, $healthCarePin, $healthCareState, $healthCareEmail, $healthCareHelpLineNo, $healthCareApntBookingNo);
+    $UpdateHealthcare = $HealthCare->updateHealthCare($imgFolder, $healthCareName, $healthCareAddress1, $healthCareAddress2, $healthCareCity, $healthCareDist, $healthCarePin, $healthCareState, $healthCareEmail, $healthCareHelpLineNo, $healthCareApntBookingNo);
     if($UpdateHealthcare){
         echo "<script>alert('Updated!')</script>";
     }else{
@@ -44,44 +44,20 @@ if (isset($_POST['update']) ==  true) {
 
 
 //Fetching Healt Care Details
-$showHelthCare = $helthCare->showhelthCare($adminId);
+$showHealthCare = $HealthCare->showhelthCare($adminId);
 
-if(!empty($showHelthCare)){
-    foreach($showHelthCare as $helthCare){
-        $helthCareLogo      = $helthCare['logo'];
-        $helthCareId        = $helthCare['hospital_id'];
-        $helthCareName      = $helthCare['hospital_name'];
-        $helthCareAddress1  = $helthCare['address_1'];
-        $helthCareAddress2  = $helthCare['address_2'];
-        $helthCareCity      = $helthCare['city'];
-        $helthCareDist      = $helthCare['dist'];
-        $helthCarePin       = $helthCare['pin'];
-        $helthCareState     = $helthCare['health_care_state'];
-        $helthCareEmail     = $helthCare['hospital_email'];
-        $helthCarePhno      = $helthCare['hospital_phno'];
-        $helthCareApntbkNo  = $helthCare['appointment_help_line'];
-    
-    }
-}else{
-    $helthCareLogo      = '';
-    $helthCareId        = '';
-    $helthCareName      = '';
-    $helthCareAddress1  = '';
-    $helthCareAddress2  = '';
-    $helthCareCity      = '';
-    $helthCareDist      = '';
-    $helthCarePin       = '';
-    $helthCareState     = '';
-    $helthCareEmail     = '';
-    $helthCarePhno      = '';
-    $helthCareApntbkNo  = '';
-}
-
-
-
-       
-
-
+$healthCareLogo      = $showHealthCare['logo'];
+$healthCareId        = $showHealthCare['hospital_id'];
+$healthCareName      = $showHealthCare['hospital_name'];
+$healthCareAddress1  = $showHealthCare['address_1'];
+$healthCareAddress2  = $showHealthCare['address_2'];
+$healthCareCity      = $showHealthCare['city'];
+$healthCareDist      = $showHealthCare['dist'];
+$healthCarePin       = $showHealthCare['pin'];
+$healthCareState     = $showHealthCare['health_care_state'];
+$healthCareEmail     = $showHealthCare['hospital_email'];
+$healthCarePhno      = $showHealthCare['hospital_phno'];
+$healthCareApntbkNo  = $showHealthCare['appointment_help_line'];
 
 ?>
 <!DOCTYPE html>
@@ -98,15 +74,15 @@ if(!empty($showHelthCare)){
     <title>SB Admin 2 - Buttons</title>
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= PLUGIN_PATH ?>fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
-    <link href="css/upload-design.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/helth-care.css">
+    <link href="<?= CSS_PATH ?>sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?= CSS_PATH ?>upload-design.css" rel="stylesheet">
+    <link href="<?= CSS_PATH ?>helth-care.css" rel="stylesheet">
 
 
 </head>
@@ -117,7 +93,7 @@ if(!empty($showHelthCare)){
     <div id="wrapper">
 
         <!-- sidebar -->
-        <?php include PORTAL_COMPONENT.'sidebar.php'; ?>
+        <?php include ROOT_COMPONENT.'sidebar.php'; ?>
         <!-- end sidebar -->
 
         <!-- Content Wrapper -->
@@ -127,7 +103,7 @@ if(!empty($showHelthCare)){
             <div id="content">
 
                 <!-- Topbar -->
-                <?php include PORTAL_COMPONENT.'topbar.php'; ?>
+                <?php include ROOT_COMPONENT.'topbar.php'; ?>
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
@@ -135,7 +111,7 @@ if(!empty($showHelthCare)){
 
                     <!-- Page Heading -->
                     <h1 class="h3 mb-4 text-gray-800">Health Care</h1>
-                   
+
                     <!-- New Section -->
                     <div class="col">
                         <div class="card shadow mb-4">
@@ -143,82 +119,102 @@ if(!empty($showHelthCare)){
                                 <h6 class="m-0 font-weight-bold text-primary">Add / Update Helth Care Details</h6>
                             </div>
                             <div class="card-body">
-                                <form action="helth-care.php" method="post" enctype="multipart/form-data">
+                                <?php if (isset($_GET['setup'])): ?>
+                                <div class="alert alert-warning" role="alert">
+                                    <?= $_GET['setup'] ?>
+                                </div>
+                                <?php endif; ?>
+                                <form action="<?= PAGE ?>" method="post" enctype="multipart/form-data">
                                     <div class="row">
                                         <div class="col-md-6">
-                                        <div >
-                                                <img class="mb-0 mt-3 rounded" src="<?php echo $helthCareLogo; ?>" width="100%" height="180" alt="">
-                                        </div>
-                                            <div class="col-md-12">
                                             <div>
-                                                <label class="mb-0 mt-1" for="name">Hospital Logo</label>
+                                                <img class="mb-0 mt-3 rounded" src="<?php echo $healthCareLogo; ?>"
+                                                    width="100%" height="180" alt="">
                                             </div>
-                                            <div class="btn btn-primary btn-sm hospital_logo">
-                                                
-                                                <input class="hospital_logo_input" name="site-logo" id="site-logo" type="file">
-                                            </div>
+                                            <div class="col-md-12">
+                                                <div>
+                                                    <label class="mb-0 mt-1" for="name">Hospital Logo</label>
+                                                </div>
+                                                <div class="btn btn-primary btn-sm hospital_logo">
+
+                                                    <input class="hospital_logo_input" name="site-logo" id="site-logo"
+                                                        type="file">
+                                                </div>
                                             </div>
                                             <div class="col-md-12">
                                                 <label class="mb-0 mt-1" for="helthcare-name">Helth Care Name</label>
-                                                <input class="form-control" type="text" name="helthcare-name" id="helthcare-name" value="<?php echo $helthCareName; ?>" required>
+                                                <input class="form-control" type="text" name="helthcare-name"
+                                                    id="helthcare-name" value="<?php echo $healthCareName; ?>" required>
                                             </div>
-        
+
                                             <div class="col-md-12">
-                                                <label class="mb-0 mt-1" for="helpline-no">Help Line Number</Address></label>
-                                                <input class="form-control" type="text" name="helpline-no" id="helpline-no" value="<?php echo $helthCarePhno; ?>" required>
+                                                <label class="mb-0 mt-1" for="helpline-no">Help Line Number</Address>
+                                                    </label>
+                                                <input class="form-control" type="text" name="helpline-no"
+                                                    id="helpline-no" value="<?php echo $healthCarePhno; ?>" required>
                                             </div>
                                             <div class="col-md-12">
-                                                <label class="mb-0 mt-1" for="apnt-booking-no">Appointment Help Line</label>
-                                                <input class="form-control" type="text" name="apnt-booking-no" id="apnt-booking-no" value="<?php echo $helthCareApntbkNo; ?>" required>
+                                                <label class="mb-0 mt-1" for="apnt-booking-no">Appointment Help
+                                                    Line</label>
+                                                <input class="form-control" type="text" name="apnt-booking-no"
+                                                    id="apnt-booking-no" value="<?php echo $healthCareApntbkNo; ?>"
+                                                    required>
                                             </div>
-                                            
+
                                         </div>
 
                                         <div class="col-md-6">
-                                        <div class="col-md-12">
+                                            <div class="col-md-12">
                                                 <label class="mb-0 mt-1" for="email">Helth Care Email</label>
-                                                <input class="form-control" type="text" name="email" id="email" value="<?php echo $helthCareEmail; ?>">
+                                                <input class="form-control" type="text" name="email" id="email"
+                                                    value="<?php echo $healthCareEmail; ?>">
                                             </div>
 
                                             <div class="col-md-12">
                                                 <label class="mb-0 mt-1" for="state">Select State</label>
                                                 <select class="form-control" name="state" id="state" required>
-                                                    <?php echo '<option value="'.$helthCareState.'">'.$helthCareState.'</option>';?>
+                                                    <?php echo '<option value="'.$healthCareState.'">'.$healthCareState.'</option>';?>
                                                     <option value="West Bengal">West Bengal</option>
                                                     <option value="Others">Others</option>
                                                 </select>
                                             </div>
                                             <div class="col-md-12">
                                                 <label class="mb-0 mt-1" for="address-1">Address 1</label>
-                                                <input class="form-control" type="text" maxlength="50" name="address-1" id="address-1" value="<?php echo $helthCareAddress1; ?>" required>
+                                                <input class="form-control" type="text" maxlength="50" name="address-1"
+                                                    id="address-1" value="<?php echo $healthCareAddress1; ?>" required>
                                             </div>
-        
+
                                             <div class="col-md-12">
                                                 <label class="mb-0 mt-1" for="address-2">Address 2</label>
-                                                <input class="form-control" type="text" maxlength="50" name="address-2" id="address-2" value="<?php echo $helthCareAddress2; ?>" required>
+                                                <input class="form-control" type="text" maxlength="50" name="address-2"
+                                                    id="address-2" value="<?php echo $healthCareAddress2; ?>" required>
                                             </div>
                                             <div class="col-md-12">
                                                 <label class="mb-0 mt-1" for="city">City</label>
-                                                <input class="form-control" type="text" maxlength="50" name="city" id="city" value="<?php echo $helthCareCity; ?>" required>
+                                                <input class="form-control" type="text" maxlength="50" name="city"
+                                                    id="city" value="<?php echo $healthCareCity; ?>" required>
                                             </div>
                                             <div class="col-md-12">
                                                 <label class="mb-0 mt-1" for="dist">Dist</label>
-                                                <input class="form-control" type="text" maxlength="50" name="dist" id="dist" value="<?php echo $helthCareDist; ?>" required>
+                                                <input class="form-control" type="text" maxlength="50" name="dist"
+                                                    id="dist" value="<?php echo $healthCareDist; ?>" required>
                                             </div>
 
                                             <div class="col-md-12">
                                                 <label class="mb-0 mt-1 ps-1" for="pin">PIN</label>
-                                                <input class="form-control" type="number" maxlength="7" minlength="7" name="pin" id="pin" value="<?php echo $helthCarePin; ?>" required>
+                                                <input class="form-control" type="number" maxlength="7" minlength="7"
+                                                    name="pin" id="pin" value="<?php echo $healthCarePin; ?>" required>
                                             </div>
                                         </div>
                                     </div>
-                                    
+
                                     <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-2 me-md-2">
-                                        <button class="btn btn-success me-md-2" name="update" type="submit">Update</button>&nbsp
+                                        <button class="btn btn-success me-md-2" name="update"
+                                            type="submit">Update</button>&nbsp
 
                                         <button class="btn btn-primary me-md-2" name="update" type="submit">ADD</button>
                                     </div>
-                                    
+
                                 </form>
                             </div>
                         </div>
@@ -232,7 +228,7 @@ if(!empty($showHelthCare)){
             <!-- End of Main Content -->
 
             <!-- Footer -->
-            <?php include PORTAL_COMPONENT.'footer-text.php'; ?>
+            <?php include ROOT_COMPONENT.'footer-text.php'; ?>
             <!-- End of Footer -->
 
         </div>
@@ -247,14 +243,14 @@ if(!empty($showHelthCare)){
     </a>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="../js/bootstrap-js-4/bootstrap.bundle.min.js"></script>
+    <script src="<?= PLUGIN_PATH ?>jquery/jquery.min.js"></script>
+    <script src="<?= JS_PATH ?>bootstrap-js-4/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?= PLUGIN_PATH ?>jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="<?= JS_PATH ?>sb-admin-2.min.js"></script>
 
 </body>
 
