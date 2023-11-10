923eb71afd035ec5e2e547887d2379cd889b4e3d
ROODRO <roodro.leelija@gmail.com>
2023-11-10 10:55:18 +0530

    stock details expiaring product tab is ok. need to defien edit button and delete button woring state.

--- stock-expiring.php
+++ stock-expiring.php
@@ -45,7 +45,7 @@ $showExpiry = $CurrentStock->showStockExpiry(NOW, $adminId);
     <title>Expiring Items</title>
 
     <!-- Custom fonts for this template-->
-    <link href="<?= ASSETS_PATH ?>fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= PLUGIN_PATH ?>fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
