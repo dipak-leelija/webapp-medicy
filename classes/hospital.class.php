e693f444d2622d77082a0a062d344932a919447c
ROODRO <roodro.leelija@gmail.com>
2023-11-16 17:29:44 +0530

    update function modified.

--- classes/hospital.class.php
+++ classes/hospital.class.php
@@ -1,29 +1,31 @@
 <?php
 
-class HealthCare extends DatabaseConnection{
+class HealthCare extends DatabaseConnection
+{
 
-    function addClinicInfo($clinicId, $adminId, $addedTime){
+    function addClinicInfo($clinicId, $adminId, $addedTime)
+    {
 
         $addClinicData = "INSERT INTO `clinic_info`(`hospital_id`, `admin_id`, `added_on`) VALUES ('$clinicId','$adminId','$addedTime')";
         $query = $this->conn->query($addClinicData);
         return $query;
-
     } //showHospital function end
 
 
 
 
 
-    function showHealthCare($adminId) {
+    function showHealthCare($adminId)
+    {
         $response = array();
-    
+
         try {
             $sql = "SELECT * FROM clinic_info WHERE `admin_id` = ?";
             $stmt = $this->conn->prepare($sql);
-    
+
             if ($stmt) {
                 $stmt->bind_param("s", $adminId);
-    
+
                 if ($stmt->execute()) {
                     $result = $stmt->get_result();
 
@@ -31,7 +33,7 @@ class HealthCare extends DatabaseConnection{
                         // Fetch all rows into an array
                         $row = $result->fetch_assoc();
                         $response = $row;
-                        
+
                         return $response;
                     } else {
                         return $response;
@@ -42,43 +44,58 @@ class HealthCare extends DatabaseConnection{
             } else {
                 throw new Exception("Error in preparing SQL statement");
             }
-    
         } catch (Exception $e) {
             // Handle any exceptions that may occur
             throw new Exception($e->getMessage());
         }
     }
-    
-    
 
-    function updateHealthCare($imgFolder, $healthCareName, $healthCareAddress1, $healthCareAddress2, $healthCareCity, $healthCareDist, $healthCarePin, $healthCareState, $healthCareEmail, $healthCareHelpLineNo, $healthCareApntBookingNo){
 
-        $updateHealthCare = "UPDATE  clinic_info SET logo = '$imgFolder', hospital_name = '$healthCareName', address_1 = '$healthCareAddress1', address_2 = '$healthCareAddress2', city = '$healthCareCity', dist = '$healthCareDist', pin = '$healthCarePin', health_care_state = '$healthCareState', hospital_email = '$healthCareEmail', hospital_phno = '$healthCareHelpLineNo', appointment_help_line = '$healthCareApntBookingNo'";
 
-        // echo $updateHealthCare.$this->conn->error;
-        // exit;
-        $updateHealthCareQuery = $this->conn->query($updateHealthCare);
-        // echo $updateHealthCareQuery.$this->conn->error;
-        // exit;
-        return $updateHealthCareQuery;
+
+    // update helthcare details function ------------------
+
+    function updateHealthCare($imgFolder, $healthCareName, $healthCareAddress1, $healthCareAddress2, $healthCareCity, $healthCareDist, $healthCarePin, $healthCareState, $healthCareEmail, $healthCareHelpLineNo, $healthCareApntBookingNo, $adminId){
+        try {
+            $updateHealthCare = "UPDATE clinic_info SET logo = ?, hospital_name = ?, address_1 = ?, address_2   = ?, city = ?, dist = ?, pin = ?, health_care_state = ?, hospital_email = ?, hospital_phno = ?,   appointment_help_line = ? WHERE admin_id = ?";
+
+            $stmt = $this->conn->prepare($updateHealthCare);
+
+            $stmt->bind_param("ssssssssssss", $imgFolder, $healthCareName, $healthCareAddress1,     $healthCareAddress2, $healthCareCity, $healthCareDist, $healthCarePin, $healthCareState,    $healthCareEmail, $healthCareHelpLineNo, $healthCareApntBookingNo, $adminId);
+
+            $stmt->execute();
+
+            $updateHealthCareQuery = $stmt->affected_rows;
+
         
-    }// end updateAppointmentsbyId function
+
+            $stmt->close();
+
+            return $updateHealthCareQuery;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+        return 0; // sql fail.
+    }
 
 
 
-        // used in text update
-        function updateHealthCareDesc($WhatWeDoText, $appointmentBookText, $subscribeText, $footerText){
-            $updateDsc = "UPDATE clinic_info SET main_desc = '$WhatWeDoText', footer_desc = '$footerText', book_appointment_text = '$appointmentBookText', subscribe_text = '$subscribeText'";
-            
-            $updateDscQuery = $this->conn->query($updateDsc);
-            // echo $updateDscQuery.$this->conn->error;
-            // exit;
-            return $updateDscQuery;
-        }//eof updateHealthCareDesc function
+
+
+    // used in text update
+    function updateHealthCareDesc($WhatWeDoText, $appointmentBookText, $subscribeText, $footerText)
+    {
+        $updateDsc = "UPDATE clinic_info SET main_desc = '$WhatWeDoText', footer_desc = '$footerText', book_appointment_text = '$appointmentBookText', subscribe_text = '$subscribeText'";
+
+        $updateDscQuery = $this->conn->query($updateDsc);
+        // echo $updateDscQuery.$this->conn->error;
+        // exit;
+        return $updateDscQuery;
+    } //eof updateHealthCareDesc function
 
 
 
-}//Hospital Class end
+} //Hospital Class end
 
 
 
@@ -89,12 +106,3 @@ class HealthCare extends DatabaseConnection{
 
 //     echo $hospitalDetails['hospital_name'];
 // }
-
-
-
-
-
-
-
-
-?>
