2eb5ef73995251df5bba5f7ab06e555cad19d66d
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-11-17 18:37:03 +0530

    returning appointment entry fixed

--- patient-selection.php
+++ patient-selection.php
@@ -26,31 +26,29 @@ $showPatients = json_decode($showPatients,true);
     <title>Enter Patient Details</title>
 
 
-    <link href="<?php echo PLUGIN_PATH ?>fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
+    <link href="<?php echo PLUGIN_PATH ?>fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
     <!-- Custom styles for this template -->
-
     <link href="<?php echo CSS_PATH ?>sb-admin-2.min.css" rel="stylesheet">
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
 
 
     <!-- Custom styles for this page -->
+    <link rel="stylesheet" href="<?php echo CSS_PATH ?>custom/appointment.css">
 
     <link href="<?php echo PLUGIN_PATH ?>datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css"> -->
+    <link href="<?= PLUGIN_PATH ?>choices/assets/styles/choices.min.css" rel="stylesheet" />
 
-    <link rel="stylesheet" href="<?php echo CSS_PATH ?>custom/appointment.css">
 
 </head>
 
 <body>
 
     <!-- Page Wrapper -->
-
     <div id="wrapper">
 
         <!-- sidebar -->
@@ -69,103 +67,89 @@ $showPatients = json_decode($showPatients,true);
 
 
                 <div class="container-fluid">
-                    <h4 class=" mb-4 mt-0"><b>Fill Returning Patient Details</b></h4>
-                    <div class="row d-flex justify-content-center">
-                        <!-- <div class="col-xl-7 col-lg-8 col-md-10 col-11 text-center"> -->
-                            <div class=" col-md-10 text-center">
-                            <div class="card mt-0">
+                    <div class="card p-0">
+                        <div class="card-header">
 
+                        </div>
+                        <div class="card-body my-5 my-md-1 p-md-5">
+                            <form class="row flex-column align-items-center" action="returning-appointment-entry.php"
+                                method="post">
 
-                                <form class="form-card" action="returning-appointment-entry.php" method="post">
-                                    <div class="row justify-content-between text-left">
-                                        <div class="col-md-12 mb-2">
-                                        <label class="form-control-label" for="patientName">Patient Name<span class="text-danger"> *</span></label>
-                                            <select class="form-control customDropSelection patient-select" data-live-search="true" name="patientName" id="patientList" onChange="getPhno()" required>
-                                                <option value="" selected disabled> Search Patient Name </option>
-                                                <?php
+                                <div class="section col-12 col-md-6">
+                                    <div class="data-test-hook=" remove-button"">
+                                        <select class="form-control " id="choices-remove-button" name="patientName"
+                                            required>
+                                            <option value="" selected disabled> Search Patient Name
+                                            </option>
+                                            <?php
                                                     foreach ($showPatients as $patientsRow) {
                                                         // $data[] = $patientsRow;
                                                         echo "<option value='".$patientsRow['patient_id']."'>".$patientsRow['patient_id']." - ".$patientsRow['name']."</option>";
                                                     }
                                                     ?>
-                                                
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <!-- <div class="row justify-content-between text-left">
-                                        <div class="col-md-12 mb-2">
-                                            <label for="">Patient Gurdian Name</label>
-                                            <input class="form-control" id="gurdianName" type="text" onChange="getGurdian()">
-                                        </div>
-                                    </div> -->
-                    
 
-                                    <div class="row justify-content-end">
-                                        <div class="form-group col-sm-4">
-                                            <button type="submit" name="proceed" class="btn-block btn-primary" >Proceed</button>
-                                        </div>
+                                        </select>
                                     </div>
-                                </form>
-                            </div>
+                                </div>
+
+                                <div class="form-group col-12 col-md-2 mt-2">
+                                    <button type="submit" name="proceed" class="btn-block btn-primary">Proceed</button>
+                                </div>
+                            </form>
                         </div>
+
                     </div>
                 </div>
-
-                <!-- Footer -->
-                <?php include ROOT_COMPONENT.'footer-text.php'; ?>
-                <!-- End of Footer -->
-
-                <!-- Bootstrap core JavaScript-->
-                <script src="<?php echo PLUGIN_PATH ?>jquery/jquery.min.js"></script>
-                <script src="<?php echo PLUGIN_PATH ?>jquery/jquery.slim.js"></script>
-                <script src="<?php echo JS_PATH ?>bootstrap-js-4/bootstrap.bundle.min.js"></script>
-                <script src="<?php echo JS_PATH ?>bootstrap-js-4/bootstrap.min.js"></script>
-                <script src="<?php echo JS_PATH ?>bootstrap-js-4/bootstrap.js"></script>
+            </div>
+        </div>
+    </div>
+    <!-- Page Wrapper end -->
 
 
+    <!-- Footer -->
+    <?php include ROOT_COMPONENT.'footer-text.php'; ?>
+    <!-- End of Footer -->
 
-                <!-- Core plugin JavaScript-->
-                <script src="<?php echo PLUGIN_PATH ?>jquery-easing/jquery.easing.min.js"></script>
-
-                <!-- Custom scripts for all pages-->
-                <script src="<?php echo JS_PATH ?>sb-admin-2.min.js"></script>
-
-                <!-- Page level plugins -->
-                <!-- <script src="vendor/chart.js/Chart.min.js"></script> -->
-
-                <!-- Page level custom scripts -->
-                <!-- <script src="js/demo/chart-area-demo.js"></script> -->
-                <!-- <script src="js/demo/chart-pie-demo.js"></script> -->
+    <!-- Bootstrap core JavaScript-->
+    <script src="<?php echo PLUGIN_PATH ?>jquery/jquery.min.js"></script>
+    <script src="<?php echo PLUGIN_PATH ?>jquery/jquery.slim.js"></script>
+    <script src="<?php echo JS_PATH ?>bootstrap-js-4/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo JS_PATH ?>bootstrap-js-4/bootstrap.min.js"></script>
+    <script src="<?php echo JS_PATH ?>bootstrap-js-4/bootstrap.js"></script>
 
 
-                
-<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/i18n/defaults-*.min.js"></script> -->
 
-<script>
+    <!-- Core plugin JavaScript-->
+    <script src="<?php echo PLUGIN_PATH ?>jquery-easing/jquery.easing.min.js"></script>
 
-// function getPhno() {
-//         var xmlhttp = new XMLHttpRequest();
-//         xmlhttp.open("GET", "ajax/patientPhno.ajax.php?patient-id=" + document.getElementById("patientList").value, false);
-//         xmlhttp.send(null);
-//         document.getElementById("gurdianName").innerHTML = xmlhttp.responseText;
-//     }
+    <!-- Custom scripts for all pages-->
+    <script src="<?php echo JS_PATH ?>sb-admin-2.min.js"></script>
 
-//     function getGurdian() {
+    <!-- Page level plugins -->
+    <!-- <script src="vendor/chart.js/Chart.min.js"></script> -->
 
-//         // document.getElementById('shiftValue').innerHTML= ("The Selected Dropdown value is: "+formid.doctorTiming[formid.doctorTiming.selectedIndex].text)
-//         var getGurdian = document.getElementById("gurdianName").value;
-//         console.log(getGurdian);
-//     }
+    <!-- Page level custom scripts -->
+    <!-- <script src="js/demo/chart-area-demo.js"></script> -->
+    <!-- <script src="js/demo/chart-pie-demo.js"></script> -->
 
+
+
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script> -->
+    <script src="<?= PLUGIN_PATH ?>choices/assets/scripts/choices.js"></script>
+
+    <script>
     //patient selection js
-    $(document).ready(function(){
-        $('.patient-select').selectpicker();
-        
-    })
+    // $(document).ready(function() {
+    //     $('.patient-select').selectpicker();
 
-
-</script>
+    // })
+    document.addEventListener('DOMContentLoaded', function() {
+        new Choices('#choices-remove-button', {
+            allowHTML: true,
+            removeItemButton: true,
+        });
+    });
+    </script>
 
 
 </body>
