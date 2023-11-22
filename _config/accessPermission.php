1b78240f626c3c481baf6b7164a6b408c1ab2a6e
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-11-22 16:20:29 +0530

    access permission page need to update

--- _config/accessPermission.php
+++ _config/accessPermission.php
@@ -1,57 +1,74 @@
 <?php
-require_once CLASS_DIR . 'dbconnect.php';
-require_once CLASS_DIR . 'employee.class.php';
-require_once CLASS_DIR . 'accessPermission.class.php';
-require_once CLASS_DIR . 'utility.class.php';
+// require_once CLASS_DIR . 'dbconnect.php';
+// require_once CLASS_DIR . 'employee.class.php';
+// require_once CLASS_DIR . 'accessPermission.class.php';
+// require_once CLASS_DIR . 'utility.class.php';
 
-$AccessPermission  = new AccessPermission;
-$Employees         = new Employees;
+// $AccessPermission  = new AccessPermission;
+// $Employees         = new Employees;
+// $Utility           = new Utility;
 
-$employeesData = $Employees->empDisplayByAdminAndEmpId($employeeId, $adminId);
-if($employeesData != null){
-    $employeesData = json_decode($employeesData);
-    $empRole = $employeesData->emp_role;
+// $currentURL        = $Utility->currentUrl();
+// $currentFile = basename($currentURL);
 
-    $permissionDetails = $AccessPermission->showPermission($empRole, $adminId);
-    $permissionDetails = json_decode($permissionDetails);
 
-    $permissonPages = [];
-    // array_push($permissonPages, LOCAL_DIR);
-    foreach($permissionDetails as $permissionDetails){
-        array_push($permissonPages, $permissionDetails->allow_page);
-    }
-    // print_r($permissonPages);
-}
+// $allowedPages = $AccessPermission->showPermission($userRole, $adminId);
+// $allowedPages = json_decode($allowedPages);
 
-if($userRole != 'ADMIN'){
+//   if ($allowedPages->status == 1) {
+//     $allowedPages = $allowedPages->data;
 
-    $flag = 0; 
+//     if(!in_array($currentFile, $allowedPages)) {
+//       echo "You are not allowed";
+//     }
+//     }
 
-    $currentURL = $_SERVER['REQUEST_URI'];
+// if($userRole != 'ADMIN'){
 
-    for($i = 0; $i<count($permissonPages); $i++){
-        if($currentURL == LOCAL_DIR.$permissonPages[$i] || $currentURL == LOCAL_DIR){
-            $flag = 1;
-            break;
-        }
-    }
+//     $flag = 0; 
 
-    if($flag == 0){
-        header("Location: {$_SERVER['HTTP_REFERER']}");
-        echo "<script>alert('Your message goes here.');</script>";
-    }
+//     $currentURL = $_SERVER['REQUEST_URI'];
+
+//     for($i = 0; $i<count($permissonPages); $i++){
+//         if($currentURL == LOCAL_DIR.$permissonPages[$i] || $currentURL == LOCAL_DIR){
+//             $flag = 1;
+//             break;
+//         }
+//     }
+
+
+/*
+    if ($flag == 0) {
+        echo '<link rel="stylesheet" href="' . CSS_PATH . 'sweetalert2/sweetalert2.min.css">';
+        echo '<script src="' . JS_PATH . 'sweetalert2/sweetalert2.all.min.js"></script>';
+        
+        echo '<script>
+                document.addEventListener("DOMContentLoaded", function () {
+                    var toastMixin = Swal.mixin({
+                        toast: true,
+                        icon: "success",
+                        title: "General Title",
+                        animation: false,
+                        position: "top-right",
+                        showConfirmButton: false,
+                        timer: 2000,
+                        timerProgressBar: true,
+                        didOpen: (toast) => {
+                            toast.addEventListener("mouseenter", Swal.stopTimer)
+                            toast.addEventListener("mouseleave", Swal.resumeTimer)
+                        }
+                    });
     
-   
-    
-   
-
-
-
+                    toastMixin.fire({
+                        title: "You have no accessable permission for this page.",
+                        icon: "warning",
+                        animation: true
+                    }).then(() => {
+                        window.location.href = "'.$_SERVER['HTTP_REFERER'].'"; 
+                    });
+                });
+              </script>';
+    }
 }
-
-
-
-
-
-
+*/
 ?>
