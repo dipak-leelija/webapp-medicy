6a539d813f00e9c6517adf37af8e5f53a0b14429
ROODRO <roodro.leelija@gmail.com>
2023-11-22 16:14:12 +0530

    working on sweetalert after redirecting.

--- _config/accessPermission.php
+++ _config/accessPermission.php
@@ -44,16 +44,33 @@ if($userRole != 'ADMIN'){
         echo '<link rel="stylesheet" href="' . CSS_PATH . 'sweetalert2/sweetalert2.min.css">';
         echo '<script src="' . JS_PATH . 'sweetalert2/sweetalert2.all.min.js"></script>';
         
-        echo '<script>window.location.href = "' . $_SERVER['HTTP_REFERER'] . '";</script>';
-        
-        $redirectFlag = 1;
+        echo '<script>
+                document.addEventListener("DOMContentLoaded", function () {
+                    var toastMixin = Swal.mixin({
+                        toast: true,
+                        icon: "success",
+                        title: "General Title",
+                        animation: false,
+                        position: "top-right",
+                        showConfirmButton: false,
+                        timer: 2000,
+                        timerProgressBar: true,
+                        didOpen: (toast) => {
+                            toast.addEventListener("mouseenter", Swal.stopTimer)
+                            toast.addEventListener("mouseleave", Swal.resumeTimer)
+                        }
+                    });
     
-        
-
+                    toastMixin.fire({
+                        title: "You have no accessable permission for this page.",
+                        icon: "warning",
+                        animation: true
+                    }).then(() => {
+                        window.location.href = "'.$_SERVER['HTTP_REFERER'].'"; 
+                    });
+                });
+              </script>';
     }
-
-
-
 }
 
 ?>
