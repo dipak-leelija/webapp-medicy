cbaa046acaf17b4af0535bec6d9f2bfc81f1bfd6
ROODRO <roodro.leelija@gmail.com>
2023-11-22 11:13:18 +0530

    working on access permission.

--- _config/accessPermission.php
+++ _config/accessPermission.php
@@ -15,17 +15,26 @@ if($employeesData != null){
     $empRole = $employeesData->emp_role;
 
     $permissionDetails = $AccessPermission->showPermission($empRole, $adminId);
+    $permissionDetails = json_decode($permissionDetails);
 }
 
 if($userRole != 'ADMIN'){
+
     print_r($permissionDetails);
+
+    // for($i=0; $i<count($permissionDetails); $i++){
+    foreach($permissionDetails as $permissionDetails){
+        echo $permissionDetails->allow_page."<br>";
+    }
 
     $currentURL = $_SERVER['REQUEST_URI'];
 
     echo "Current URL: $currentURL";
     echo "<br>";
-    echo URL;
-    
+    echo LOCAL_DIR;
+
+
+
 }
 
 
