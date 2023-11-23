d87da25cf3354aacf46e891b54b9e05e422f199b
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-11-23 11:40:16 +0530

    admin password encryption using custom function is done

--- classes/encrypt.inc.php
+++ classes/encrypt.inc.php
@@ -12,18 +12,18 @@ function get_rnd_iv($iv_len)
    return $iv;
 }
 
-function pass_enc($plain_text, $password, $iv_len = 16){
+function pass_enc($plain_text, $passkey, $iv_len = 16){
 
    $plain_text .= "\x13";
    $n = strlen($plain_text);
    if ($n % 16) $plain_text .= str_repeat("\0", 16 - ($n % 16));
    $i = 0;
    $enc_text = get_rnd_iv($iv_len);
-   $iv = substr($password ^ $enc_text, 0, 512);
+   $iv = substr($passkey ^ $enc_text, 0, 512);
    while ($i < $n) {
        $block = substr($plain_text, $i, 16) ^ pack('H*', md5($iv));
        $enc_text .= $block;
-       $iv = substr($block . $iv, 0, 512) ^ $password;
+       $iv = substr($block . $iv, 0, 512) ^ $passkey;
        $i += 16;
    }
    return base64_encode($enc_text);
@@ -35,17 +35,17 @@ function pass_enc($plain_text, $password, $iv_len = 16){
 // $pass  = USER_PASS;
 // echo pass_enc($p, $pass);
 
-function pass_dec($enc_text, $password, $iv_len = 16)
+function pass_dec($enc_text, $passkey, $iv_len = 16)
 {
    $enc_text = base64_decode($enc_text);
    $n = strlen($enc_text);
    $i = $iv_len;
    $plain_text = '';
-   $iv = substr($password ^ substr($enc_text, 0, $iv_len), 0, 512);
+   $iv = substr($passkey ^ substr($enc_text, 0, $iv_len), 0, 512);
    while ($i < $n) {
        $block = substr($enc_text, $i, 16);
        $plain_text .= $block ^ pack('H*', md5($iv));
-       $iv = substr($block . $iv, 0, 512) ^ $password;
+       $iv = substr($block . $iv, 0, 512) ^ $passkey;
        $i += 16;
    }
    return preg_replace('/\\x13\\x00*$/', '', $plain_text);
