000cb0ae51a912bc9e0a79440f640b6407d41c0f
ROODRO <roodro.leelija@gmail.com>
2023-11-30 18:11:08 +0530

    profile setup working fine.

--- _config/form-submission/profileSetup-form.php
+++ _config/form-submission/profileSetup-form.php
@@ -16,48 +16,90 @@ require_once CLASS_DIR . 'empRole.class.php';
 $Admin = new Admin;
 $Employees = new Employees;
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    if(isset($_POST['submit'])){
-
-        $imageName         = $_FILES['profile-image']['name'];
-        $tempImgName       = $_FILES['profile-image']['tmp_name'];
-
-
-        $fname = $_POST['fname'];
-        $lname = $_POST['lname'];
-        $userName = $_POST['user-name'];
-        $email = $_POST['email'];
-        $phNo = $_POST['mobile-number'];
-
-        $pss = $_POST['password'];
-        $hashedPassword = password_hash($pss, PASSWORD_DEFAULT);
-
-        $cnfPass = $_POST['cpassword'];
-        $address = $_POST['address'];
-
-
-        if($_SESSION['ADMIN']){
-            
-            $updateAdminData = $Admin->updateAdminDetails($fname, $lname, $imageName, $userName, $hashedPassword, $email, $phNo,  $address, NOW, $adminId);
-
-            if($updateAdminData['result']){
-
-                $imgFolder = ADM_IMG_DIR.$imageName;
-                move_uploaded_file($tempImgName, $imgFolder);
-            }
-
-        } else {
-                
-            $updateEmployeeData = $Employees->updateEmpData($fname.$lname, $imageName, $email, $phNo, $address, $userName, $hashedPassword, NOW, $employeeId, $adminId);
-
-            if($updateEmployeeData['result']){
-
-                $imgFolder = EMP_IMG_DIR.$imageName;
-                move_uploaded_file($tempImgName, $imgFolder);
-
-            }
-        } 
-    }
-}
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add New Product</title>
+    <script src="<?php echo JS_PATH ?>sweetAlert.min.js"></script>
+</head>
+
+<body>
+    <div>
+    </div>
+
+    <?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST['submit'])) {
+
+            $imageName         = $_FILES['profile-image']['name'];
+            $tempImgName       = $_FILES['profile-image']['tmp_name'];
+
+
+            $fname = $_POST['fname'];
+            $lname = $_POST['lname'];
+            $userName = $_POST['user-name'];
+            $email = $_POST['email'];
+            $phNo = $_POST['mobile-number'];
+
+            $pss = $_POST['password'];
+            $hashedPassword = password_hash($pss, PASSWORD_DEFAULT);
+
+            $cnfPass = $_POST['cpassword'];
+            $address = $_POST['address'];
+
+
+            if ($_SESSION['ADMIN']) {
+
+                $updateAdminData = $Admin->updateAdminDetails($fname, $lname, $imageName, $userName, $hashedPassword, $email, $phNo,  $address, NOW, $adminId);
+
+                if ($updateAdminData['result']) {
+
+                    $imgFolder = ADM_IMG_DIR . $imageName;
+                    move_uploaded_file($tempImgName, $imgFolder);
+                }
+            } else {
+
+                $updateEmployeeData = $Employees->updateEmpData($fname . $lname, $imageName, $email, $phNo, $address, $userName, $hashedPassword, NOW, $employeeId, $adminId);
+
+                if ($updateEmployeeData['result']) {
+
+                    $imgFolder = EMP_IMG_DIR . $imageName;
+                    move_uploaded_file($tempImgName, $imgFolder);
+                }
+            }
+
+            if ($updateAdminData['result'] || $updateEmployeeData['result']) {
+    ?>
+                <script>
+                    swal("Success", "Data Updated!", "success")
+                        .then((value) => {
+                            window.location = '<?php echo LOCAL_DIR ?>profile.php';
+                        });
+                </script>
+            <?php
+            } else {
+            ?>
+                <script>
+                    swal("Error", "Updation Fails!", "error")
+                        .then((value) => {
+                            window.location = '<?php echo LOCAL_DIR ?>profiles.php';
+                        });
+                </script>
+    <?php
+            }
+        }
+    }
+
+    ?>
+
+</body>
+
+</html>
