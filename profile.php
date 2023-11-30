63530e9c80e99d11d71ff4757ed9db572c0d7b5d
Jayanta Barman <jbleelija@gmail.com>
2023-11-30 16:04:53 +0530

    profile page working

--- profile.php
+++ profile.php
@@ -170,7 +170,7 @@ if ($_SESSION['ADMIN']) {
 
                                         <div class=" d-flex justify-content-center align-items-center mb-5">
                                             <!-- <div class="position-relative"> -->
-                                            <img class="img-uv-view rounded-circle" style="width: 20%;" src="<?= ASSETS_PATH ?>images/undraw_profile.svg" alt="">
+                                            <img class="img-uv-view rounded-circle" style="width: 20%;" src="<?= ($image) ? $image : ASSETS_PATH ?>images/undraw_profile.svg" alt="">
                                             <div class="position-absolute translate-middle">
                                                 <input type="file" style="display:none;" id="img-uv-input" accept="image/*" name="site-logo">
                                                 <label for="img-uv-input" class="btn btn-sm btn-outline-secondary ml-5" style="margin-top: 115%;"><i class="fas fa-edit"></i></label>
