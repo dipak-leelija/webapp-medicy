798e7cfa130d8ed645ed96c2a6db299e6b9158f1
Jayanta Barman <jbleelija@gmail.com>
2023-12-04 17:32:43 +0530

    product view , product edit page update

--- ajax/product-view-modal.ajax.php
+++ ajax/product-view-modal.ajax.php
@@ -1,15 +1,15 @@
 <?php
-require_once dirname(__DIR__).'/config/constant.php';
-require_once ROOT_DIR.'_config/sessionCheck.php';
+require_once dirname(__DIR__) . '/config/constant.php';
+require_once ROOT_DIR . '_config/sessionCheck.php';
 
-require_once CLASS_DIR.'dbconnect.php';
-require_once CLASS_DIR."products.class.php";
-require_once CLASS_DIR."quantityUnit.class.php";
-require_once CLASS_DIR."packagingUnit.class.php";
-require_once CLASS_DIR."itemUnit.class.php";
-require_once CLASS_DIR."productsImages.class.php";
-require_once CLASS_DIR."manufacturer.class.php";
-require_once CLASS_DIR."currentStock.class.php";
+require_once CLASS_DIR . 'dbconnect.php';
+require_once CLASS_DIR . "products.class.php";
+require_once CLASS_DIR . "quantityUnit.class.php";
+require_once CLASS_DIR . "packagingUnit.class.php";
+require_once CLASS_DIR . "itemUnit.class.php";
+require_once CLASS_DIR . "productsImages.class.php";
+require_once CLASS_DIR . "manufacturer.class.php";
+require_once CLASS_DIR . "currentStock.class.php";
 
 $Products       = new Products();
 $PackagingUnits = new PackagingUnits();
@@ -67,7 +67,6 @@ $QuantityUnit   = new QuantityUnit;
             align-items: center;
             justify-content: center;
         }
-
     </style>
 </head>
 
@@ -82,17 +81,19 @@ $QuantityUnit   = new QuantityUnit;
         $image          = $ProductImages->showImageById($_GET['id']);
         // print_r($image);
 
+        $Images = array();
         if ($image != NULL) {
-            foreach($image as $image){
-                $Images = $image['image'];
+            foreach ($image as $image) {
+                $Images[] = $image['image'];
             }
-            
+            // print_r($Images);
+
             if ($Images == NULL) {
                 $Images = "medicy-default-product-image.jpg";
-            }   
-        } 
-        
-        if($image == NULL){
+            }
+        }
+
+        if ($image == NULL) {
             $Images = "medicy-default-product-image.jpg";
         }
 
@@ -100,7 +101,7 @@ $QuantityUnit   = new QuantityUnit;
 
         $itemQuantityUnit = $QuantityUnit->quantityUnitName($product[0]['unit_id']);
         $itemQuantityUnit = json_decode($itemQuantityUnit, true);
-        if($itemQuantityUnit){
+        if ($itemQuantityUnit) {
             if (isset($itemQuantityUnit['data']['short_name'])) {
                 $qantityName = $itemQuantityUnit['data']['short_name'];
             } else {
@@ -109,7 +110,7 @@ $QuantityUnit   = new QuantityUnit;
         }
 
         $itemUnitName = $ItemUnit->itemUnitName($product[0]['unit']);
-        
+
     ?>
 
         <div class="container-fluid d-flex justify-content-center mt-2">
@@ -117,18 +118,23 @@ $QuantityUnit   = new QuantityUnit;
                 <div class="col-12 col-sm-4">
                     <div class="">
                         <div class="text-center border d-flex justify-content-center">
-                            <img src="<?= PROD_IMG_PATH ?><?php echo $Images; ?>" class="rounded ob-cover animated--grow-in" id="main-img" alt="...">
+                            <img src="<?= PROD_IMG_PATH ?><?php echo $Images[0]; ?>" class="rounded ob-cover animated--grow-in" id="main-img" alt="...">
                         </div>
                         <div class="row height-3 mt-2 justify-content-center">
-                            <div class="col-2 border p-0">
-                                <img src="<?= PROD_IMG_PATH ?><?php echo $Images; ?>" id="front-img" onclick="setImg(this.id)" class="rounded ob-cover h-100" alt="...">
+                            <?php foreach ($Images as $index => $imagePath) : ?>
+                                <div class="col-2 border p-0">
+                                    <img src="<?= PROD_IMG_PATH ?><?php echo $imagePath; ?>" id="img-<?php echo $index; ?>" onclick="setImg(this.id)" class="rounded ob-cover h-100" alt="...">
+                                </div>
+                            <?php endforeach; ?>
+                            <!-- <div class="col-2 border p-0">
+                                <img src="<?= PROD_IMG_PATH ?><?php echo $Images[0]; ?>" id="front-img" onclick="setImg(this.id)" class="rounded ob-cover h-100" alt="...">
                             </div>
                             <div class="col-2 border p-0" id="back-div">
-                                <img src="<?= PROD_IMG_PATH ?><?php echo $Images; ?>" id="back-img" onclick="setImg(this.id)" class="rounded ob-cover h-100" alt="...">
+                                <img src="<?= PROD_IMG_PATH ?><?php echo $Images[1]; ?>" id="back-img" onclick="setImg(this.id)" class="rounded ob-cover h-100" alt="...">
                             </div>
                             <div class="col-2 border p-0" id="side-div">
-                                <img src="<?= PROD_IMG_PATH ?><?php echo $Images; ?>" id="side-img" onclick="setImg(this.id)" class="rounded ob-cover h-100" alt="...">
-                            </div>
+                                <img src="<?= PROD_IMG_PATH ?><?php echo $Images[2]; ?>" id="side-img" onclick="setImg(this.id)" class="rounded ob-cover h-100" alt="...">
+                            </div> -->
                         </div>
                     </div>
                 </div>
@@ -140,7 +146,7 @@ $QuantityUnit   = new QuantityUnit;
                                 <h7><?php echo $manuf->name; ?></h7>
                                 <h5 class="fs-5 fst-normal">₹ <?php echo $product[0]['mrp']; ?><span class="fs-6 fw-light"><small> MRP</small></span></h5>
                                 <p class="fst-normal"><?php echo $product[0]['unit_quantity']; ?>
-                                    <?= $qantityName.' '.$itemUnitName ?>/<?php echo $pack[0]['unit_name']; ?></p>
+                                    <?= $qantityName . ' ' . $itemUnitName ?>/<?php echo $pack[0]['unit_name']; ?></p>
                                 <p>
                                     <small>
                                         <mark>
@@ -219,6 +225,7 @@ $QuantityUnit   = new QuantityUnit;
     <script>
         const setImg = (id) => {
             img = document.getElementById(id).src;
+            console.log(img);
             document.getElementById("main-img").src = img;
         }
 
@@ -238,7 +245,7 @@ $QuantityUnit   = new QuantityUnit;
                     text: 'Current Stock have this product.'
                 })
             }
-            
+
             if (btnVal == 0) {
                 swal.fire({
                         title: "Are you sure?",
