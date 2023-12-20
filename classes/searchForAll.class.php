c881c6ee597f735ba0e89bc541060ea0d9f1788b
Jayanta Barman <jbleelija@gmail.com>
2023-12-20 13:16:25 +0530

    all search page updated

--- classes/searchForAll.class.php
+++ classes/searchForAll.class.php
@@ -2,50 +2,29 @@
 
 class SearchForAll extends DatabaseConnection
 {
-    function searchAllFilter($searchData, $adminId)
-    {
-        $resultData = array(); 
-        $appointmentsResultData = array();
-        $patientsResultData = array();
-        $searchPattern = "%".$searchData."%";
+function searchAllFilter($searchData, $adminId)
+{
+    $resultData = array();
+    $appointmentsResultData = array();
+    $searchPattern = "%".$searchData."%";
 
-        try {
-            // ===== QUERY FOR APPOINTMENTS TABLE =====
-            $searchAllForAppointments = "SELECT * FROM appointments WHERE `admin_id` = '$adminId' AND `appointment_id` LIKE '%$searchData%'";
+    try {
+        // ===== QUERY FOR APPOINTMENTS TABLE =====
+        $searchAllForAppointments = "SELECT * FROM appointments WHERE `admin_id` = ? AND `appointment_id` LIKE ?";
 
-            $appointmentsStatement = $this->conn->query($searchAllForAppointments);
+        $stmt = $this->conn->prepare($searchAllForAppointments);
+        $stmt->bind_param("ss",$adminId, $searchPattern);
+        $stmt->execute();
+        $appointmentsStatement = $stmt->get_result();
 
-            var_dump($appointmentsStatement);
-
-            while ($appointmentsResult = $appointmentsStatement->fetch_assoc()) {
-                $appointmentsResultData[] = $appointmentsResult;
-            }
-
-
-
-
-            // // ===== QUERY FOR APPOINTMENTS TABLE =====
-            // $searchAllForPatients = "SELECT * FROM patient_details WHERE `admin_id`= ? AND
-            //                                 `patient_id` LIKE CONCAT('%', ?, '%') OR
-            //                                 `phno` LIKE CONCAT('%', ?, '%')";
-
-            // $patientsStatement = $this->conn->prepare($searchAllForPatients);
-            
-            // $patientsStatement->bind_param("sss", $adminId, $searchData, $searchData);
-            // $patientsStatement->execute();
-
-            // $patientsQueryResult = $patientsStatement->get_result();
-
-            // while ($patientsResult = $patientsQueryResult->fetch_assoc()) {
-            //     $patientsResultData[] = $patientsResult;
-            // }
-
-            // $resultData = array_merge($appointmentsResultData, $patientsResultData);
-
-            return json_encode(['status' => '1', 'message' => 'Data found', 'data' => $appointmentsResultData]);
-
-        } catch (Exception $e) {
-            return json_encode(['status' => '0', 'message' => $e->getMessage(), 'data' => '']);
+        while ($appointmentsResult = $appointmentsStatement->fetch_assoc()) {
+            $appointmentsResultData[] = $appointmentsResult;
         }
+
+        return json_encode(['status' => '1', 'message' => 'Data found', 'data' => $appointmentsResultData]);
+    } catch (Exception $e) {
+        return json_encode(['status' => '0', 'message' => $e->getMessage(), 'data' => '']);
     }
+}
+
 }
