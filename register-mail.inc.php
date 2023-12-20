5abbe4567e63548a2f50450b69b20ac517bdbe37
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-12-20 23:42:42 +0530

    mail testing

--- register-mail.inc.php
+++ register-mail.inc.php
@@ -1,31 +1,9 @@
 <?php
 include_once __DIR__ . "/config/constant.php";
-// include_once __DIR__ . "/includes/content.inc.php";
-// include_once __DIR__ . "/includes/contact-us-email.inc.php";
-// require_once __DIR__ . "/classes/encrypt.inc.php";
 require_once CLASS_DIR .'dbconnect.php';
 require_once __DIR__.'/PHPMailer/PHPMailer.php';
 
-// require_once __DIR__ . "/classes/class.phpmailer.php";
-// require_once __DIR__ . "/mail-templates/welcome-mail-template.php";
-
-// require_once __DIR__ . "/classes/contact.class.php";
-// require_once __DIR__ . "/classes/emails.class.php"; 
-// include_once __DIR__ . "/classes/customer.class.php";
-// include_once __DIR__ . "/classes/subscriber.class.php";
-// include_once __DIR__ . "/classes/encrypt.inc.php";
-// require_once __DIR__ . "/classes/utility.class.php"; 
-// include_once __DIR__ . "/classes/error.class.php";
-// include_once __DIR__ . "/classes/utilityMesg.class.php";
-
 $PHPMailer		= new PHPMailer();
-// $contact 		= new Contact();
-// $Customer		= new Customer();
-// $subscriber 	= new EmailSubscriber();
-// $error			= new MyError();
-// $emailObj		= new Emails();
-// $uMesg			= new MesgUtility();
-// $utility		= new Utility();
 
 $_SESSION['vkey']		= 87677;
 $_SESSION['fisrt-name'] = 'Dipak';
@@ -34,108 +12,61 @@ $_SESSION['email']		= 'dipakmajumdar.leelija@gmail.com';
 
 if(isset($_SESSION['vkey']) && isset($_SESSION['fisrt-name']) && isset($_SESSION['email'])){	
 
-	// $regAction			= '';
-	// if (isset($_SESSION[PACK_ORD])) {
-	// 	$regAction		= strip_tags(trim($_SESSION[PACK_ORD][0]));
-	// }
-
 	$verifyKey  	= strip_tags(trim($_SESSION['vkey'])); 
 	$firstName 		= strip_tags(trim($_SESSION['fisrt-name']));
 	$txtEmail 		= strip_tags(trim($_SESSION['email']));
 
-		//$subsRes = $subscriber->addSubscriber($txtContactEmail, 0, $txtContactName, '', 0, '', $txtContactPhone);
+	$sess_arr	= array('vkey', 'newCustomerSess', 'fisrt-name', 'last-name', 'profession');
+	$utility->delSessArr($sess_arr);			
+	$uMesg->dispMesgWithMesgVal(SUCONTACT001,"SUCCESS","images/icon/",'','error-block','success-block');
 
+	$msgBody = "Dear $firstName,
+			<br>
+			Welcome to Medicy! We're thrilled to have you on board.
+			<br>
+			To ensure the security of your account, please use the following One-Time Password (OTP) for verification:
+			<br>
+			<br>
+			<b>Your Verification OTP: $verifyKey<b>
+			<br>
+			<br>
+			Please enter this code to complete the registration process. If you didn't sign up for an account on Medicy, please ignore this email.
+			<br>
+			Thank you for choosing Medicy. We look forward to providing you with the best Healthcare System experience";
 
-		// $verifyKey = base64_encode($verifyKey);
-		// $regAction = base64_encode($regAction);
-		// $verificationUrl = URL.'/verify-account.php?action='.$regAction.'&verify='.$verifyKey;
-		// Contact Data inser in contact table
-		// $contact->addContact($firstName.' '.$lastName, $txtEmail, '', '');		
-
-		// $cusId			= $utility->returnSess('userid', 0);
-			
-		// $sess_arr	= array('vkey', 'newCustomerSess', 'fisrt-name', 'last-name', 'profession');
-		// $utility->delSessArr($sess_arr);			
-		//$uMesg->dispMesgWithMesgVal(SUCONTACT001,"SUCCESS","images/icon/",'','error-block','success-block');
-			
-
-
-
-
-		/*===================================================================================================
-		|																									|
-		|									    send mail to new customer									|
-		|																									|
-		====================================================================================================*/
-
-		// echo $encLink =  URL.'/verify-account.php?verify='.$vkey;
-
-		// $userMailBody =  welcomeMailToUser($firstName, $verificationUrl);
-		// $fullName = $firstName.' '.$lastName; 
-		try {
-			$PHPMailer->IsSendmail();
-			$PHPMailer->IsHTML(true);
-			$PHPMailer->Host        = gethostname();
-			$PHPMailer->SMTPAuth    = true;
-			$PHPMailer->Username    = SITE_EMAIL;
-			$PHPMailer->Password    = SITE_EMAIL_P;
-			$PHPMailer->From        = SITE_EMAIL;
-			$PHPMailer->FromName    = SITE_NAME;
-			$PHPMailer->Sender      = SITE_EMAIL;
-			$PHPMailer->addAddress($txtEmail, $firstName);
-			$PHPMailer->Subject     = "Verification Code of ". SITE_NAME;
-			$PHPMailer->Body        = "The Code id $verifyKey";
-			if (!$PHPMailer->send()) {
-
-				echo "Message could not be sent to customer. Mailer Error:-> {$PHPMailer->ErrorInfo}<br>";
-				exit;
-			}
+	/*===================================================================================================
+	|									    send mail to new customer									|
+	====================================================================================================*/
+ 
+	try {
+		$PHPMailer->IsSendmail();
+		$PHPMailer->IsHTML(true);
+		$PHPMailer->Host        = gethostname();
+		$PHPMailer->SMTPAuth    = true;
+		$PHPMailer->Username    = SITE_EMAIL;
+		$PHPMailer->Password    = SITE_EMAIL_P;
+		$PHPMailer->From        = SITE_EMAIL;
+		$PHPMailer->FromName    = SITE_NAME;
+		$PHPMailer->Sender      = SITE_EMAIL;
+		$PHPMailer->addAddress($txtEmail, $firstName);
+		$PHPMailer->Subject     = "Account Verification OTP - ". SITE_NAME;
+		$PHPMailer->Body        = $msgBody;
+		
+		if (!$PHPMailer->send()) {
+			echo "Message could not be sent to customer. Mailer Error:-> {$PHPMailer->ErrorInfo}<br>";
+		}else {
+			echo 'mail sent';	
+		}
 			$PHPMailer->clearAllRecipients();
 
-		} catch (Exception $e) {
-			echo "Message could not be sent. Mailer Error:-> {$PHPMailer->ErrorInfo}";
-		}
-
-
-
-		/*===============================================================================================
-		|																								|
-		|										send mail to admin						    		    |
-		|																								|
-		================================================================================================*/
-			
-	// 	$data 		= array('Name', 'Email', 'Profession');
-	// 	$data_val 	= array($firstName.' '.$lastName, $txtEmail, $profession);
-
-	// 	$adminMailBody =  welcomeMailToAdmin($data, $data_val);
-
-	// 	try {
-	// 		$PHPMailer->IsSendmail();
-	// 		$PHPMailer->IsHTML(true);
-	// 		$PHPMailer->Host        = gethostname();
-	// 		$PHPMailer->SMTPAuth    = true;
-	// 		$PHPMailer->Username    = SITE_EMAIL;
-	// 		$PHPMailer->Password    = SITE_EMAIL_P;
-	// 		$PHPMailer->From        = SITE_EMAIL;
-	// 		$PHPMailer->FromName    = COMPANY_FULL_NAME;
-	// 		$PHPMailer->Sender      = SITE_EMAIL;
-	// 		$PHPMailer->addAddress(SITE_EMAIL, COMPANY_S.' Admin');
-	// 		$PHPMailer->Subject     = "New User Resgisterd to -". COMPANY_FULL_NAME;
-	// 		$PHPMailer->Body        = $adminMailBody;
-	// 		if (!$PHPMailer->send()) {
-	// 			echo "Message could not be sent to admin. Mailer Error:-> {$PHPMailer->ErrorInfo}<br>";
-	// 			exit;
-	// 		}
-	// 		$PHPMailer->clearAllRecipients();
-	// 	} catch (Exception $e) {
-	// 		echo "Message could not be sent. Mailer Error:-> {$PHPMailer->ErrorInfo}";
-	// 	}
-		
-	// 	echo "<span style='color:green;'>".SUCONTACT001."</span>";	
+	} catch (Exception $e) {
+		echo "Message could not be sent. Mailer Error:-> {$PHPMailer->ErrorInfo}";
 	}
+
+}
 	
 
-header("location: verification-sent.php");
-exit;
+// header("location: verification-sent.php");
+// exit;
 
 ?>
