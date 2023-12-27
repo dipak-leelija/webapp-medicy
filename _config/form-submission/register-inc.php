b4c133ef213db900a2e1c69f19a8042089c44049
ROODRO <roodro.leelija@gmail.com>
2023-12-27 12:22:57 +0530

    create new file for opt verification during registration time.

---
+++ _config/form-submission/register-inc.php
@@ -0,0 +1,39 @@
+<?php
+include_once dirname(dirname(__DIR__)) . "/config/constant.php";
+require_once CLASS_DIR . 'dbconnect.php';
+require_once CLASS_DIR . 'admin.class.php';
+
+$Admin = new Admin;
+
+if ($_SESSION['vkey'] && $_SESSION['adm_id'] && $_SESSION['last_activity'] && $_SESSION['time_out']) {
+
+   
+    $Key = $_SESSION['vkey'];
+    $admId = $_SESSION['adm_id'];
+    $status = '0';
+
+    if (isset($_POST['otp-submit'])) {
+      
+        $chkOtp = $_POST['digit1'].$_POST['digit2'].$_POST['digit3'].$_POST['digit4'].$_POST['digit5'].$_POST['digit6'];
+
+        if ($chkOtp == $Key) {
+            $status = '1';
+
+            $admStatusUpdate = $Admin->updateAdminStatus($admId, $status);
+            // print_r($admStatusUpdate);
+
+            if ($admStatusUpdate['result']) {
+                
+                header("Location: " . LOCAL_DIR . "login.php");
+                exit; 
+
+            } else {
+                echo "Status update failed.";
+            }
+
+        } else {
+            echo "Invalid OTP.";
+        }
+    }
+}
+?>
