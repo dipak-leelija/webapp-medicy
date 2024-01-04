7723524f3284dee1e288905d53664139b306f18f
ROODRO <roodro.leelija@gmail.com>
2024-01-04 13:03:39 +0530

    rename file ajax/email-verification-validation.ajax.php as ajax/admin-mail-usrnm-existance-check.ajax.php. admin username and email existance checked on input fuild focous out.

---
+++ ajax/admin-mail-usrnm-existance-check.ajax.php
@@ -0,0 +1,49 @@
+<?php
+require_once dirname(__DIR__) . '/config/constant.php';
+
+require_once CLASS_DIR . 'dbconnect.php';
+require_once CLASS_DIR . 'admin.class.php';
+require_once CLASS_DIR . 'employee.class.php';
+
+
+
+$Admin = new Admin;
+$Employee = new Employees;
+
+
+// =========== admin email existance check =============
+
+if (isset($_POST['chekEmailExistance'])) {
+    $checkEmail = $_POST['chekEmailExistance'];
+
+    $checkAdminMailExistance = $Admin->echeckEmail($checkEmail);
+
+    $empCol1 = 'emp_email';
+    $checkEmpMailExistance = json_decode($Employee->selectEmpByCol($empCol1, $checkEmail));
+
+    if ($checkAdminMailExistance == 1 || $checkEmpMailExistance->status == 1) {
+        echo '1';
+    } else {
+        echo '0';
+    }
+}
+
+
+// =========== admin username existance check =============
+
+if (isset($_POST['chekUsrnmExistance'])) {
+    $checkUsername = $_POST['chekUsrnmExistance'];
+
+    $checkAdminMailExistance = $Admin->echeckUsername($checkUsername);
+
+    $empCol1 = 'emp_username';
+    $checkEmpMailExistance = json_decode($Employee->selectEmpByCol($empCol1, $checkUsername));
+
+    if ($checkAdminMailExistance == 1 || $checkEmpMailExistance->status == 1) {
+        echo '1';
+    } else {
+        echo '0';
+    }
+}
+
+
