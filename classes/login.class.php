155b1f2a1d9b311171761ce915f61c000871158a
ROODRO <roodro.leelija@gmail.com>
2024-01-09 15:06:25 +0530

    modified login check logic.

--- classes/login.class.php
+++ classes/login.class.php
@@ -10,86 +10,32 @@ class LoginForm extends DatabaseConnection
     function login($email, $password, $roleData)
     {
 
-        $admSql1 = "SELECT * FROM `admin` WHERE (`email` = '$email' OR `username` = '$email') AND `reg_status`= 1";
+        // ======== employee login =========
+        $sql = "SELECT * FROM `employees` WHERE `emp_email` = '$email' OR `emp_username` = '$email'";
+        $result = $this->conn->query($sql);
 
-        $result1 = $this->conn->query($admSql1);
+        if ($result->num_rows > 0) {
+            while ($data = $result->fetch_object()) {
 
-        // ======== unsuccessfull admin registration process =========
-        if ($result1->num_rows == 0) {
-
-            $admSql2 = "SELECT * FROM `admin` WHERE (`email` = '$email' OR `username` = '$email') AND `reg_status`= 0";
-
-            $result2 = $this->conn->query($admSql2);
-
-            if ($result2->num_rows > 0) {
-
-                $OTP = otpGgenerator();
-                $timeout_duration = 600; // 10 * 60 seconds
-
-                while ($admData2 = $result2->fetch_object()) {
-                    // print_r($admData2);
-
-                    $dbPasshash = $admData2->password;
-                    $x_password = pass_dec($dbPasshash, ADMIN_PASS);
-
-                    if ($x_password === $password) {
-
-                        session_start();
-
-                        $_SESSION['REGISTRATION']       = true;
-                        $_SESSION['ADMIN_REGISER']      = true;
-                        $_SESSION['PRIMARY_REGISTER']   = false;
-                        $_SESSION['SECONDARY_REGISTER'] = true;
-                        $_SESSION['session_start_time'] = date('H:i:s');
-                        $_SESSION['time_out']           = $timeout_duration;
-                        $_SESSION['verify_key']         = $OTP;
-                        $_SESSION['first-name']         = $admData2->fname;
-                        $_SESSION['email']              = $admData2->email;
-                        $_SESSION['username']           = $admData2->username;
-                        $_SESSION['adm_id']             = $admData2->admin_id;
-
-                        header("Location: register-mail.inc.php");
-                        exit;
-                    }
-
-                    return 'Wrong Password';
-                }
-            }
-            header("Location: register.php");
-            exit;
-        }
-
-        // ======== admin login =========
-        if ($result1->num_rows > 0) {
-            while ($admData1 = $result1->fetch_object()) {
-
-                $dbPasshash = $admData1->password;
-                $x_password = pass_dec($dbPasshash, ADMIN_PASS);
-                // exit;
+                $dbPasshash = $data->emp_password;
+                $x_password = pass_dec($dbPasshash, EMP_PASS);
 
                 if ($x_password === $password) {
 
                     session_start();
-                    $_SESSION['LOGGEDIN']           = true;
-                    $_SESSION['ADMIN']              = true;
-                    $_SESSION['USER_TYPE']          = 'NOT USER';
-                    $_SESSION['ADMIN_EMAIL']        = $admData1->email;
-                    $_SESSION['USER_ROLE']          = 'ADMIN';
-                    $_SESSION['ADMIN_FNAME']        = $admData1->fname;
-                    $_SESSION['ADMIN_LNAME']        = $admData1->lname;
-                    $_SESSION['ADMIN_IMG']          = $admData1->adm_img;
-                    $_SESSION['ADMIN_CONTACT_NO']   = $admData1->mobile_no;
-                    $_SESSION['ADMIN_USERNAME']     = $admData1->username;
-                    $_SESSION['ADMIN_PASSWORD']     = $admData1->password;
-                    $_SESSION['ADMIN_ADDRESS']      = $admData1->address;
-                    $_SESSION['ADMINID']            = $admData1->admin_id;
-
-
-                    // Insert login time into login_time table
-                    $adminId = $_SESSION['ADMINID'];
-                    $loginTime = date('Y-m-d H:i:s');
-                    $this->insertLoginTime($adminId, $loginTime);
-
+                    $_SESSION['LOGGEDIN']       = true;
+                    $_SESSION['ADMIN']          = false;
+                    $_SESSION['USER_TYPE']      = 'USER';
+                    $_SESSION['EMP_EMAIL']      = $email;
+                    $_SESSION['EMP_CONTACT_NO'] = $data->emp_contact_no;
+                    $_SESSION['EMP_ROLE']       = $data->emp_role;
+                    $_SESSION['EMP_NAME']       = $data->emp_name;
+                    $_SESSION['EMP_IMG']        = $data->emp_img;
+                    $_SESSION['EMP_ADDRESS']    = $data->emp_address;
+                    $_SESSION['EMP_USERNAME']   = $data->emp_username;
+                    $_SESSION['EMP_PASSWORD']   = $data->emp_password;
+                    $_SESSION['EMPID']          = $data->emp_id;
+                    $_SESSION['ADMIN_ID']       = $data->admin_id;
 
                     header("Location: " . URL);
                     exit;
@@ -98,32 +44,87 @@ class LoginForm extends DatabaseConnection
                 }
             }
         } else {
-            // ======== employee login =========
-            $sql = "SELECT * FROM `employees` WHERE `emp_email` = '$email' OR `emp_username` = '$email'";
-            $result = $this->conn->query($sql);
 
-            if ($result->num_rows > 0) {
-                while ($data = $result->fetch_object()) {
+            // =============== ADMIN LOGIN ===============
+            $admSql1 = "SELECT * FROM `admin` WHERE (`email` = '$email' OR `username` = '$email') AND `reg_status`= 1";
 
-                    $dbPasshash = $data->emp_password;
-                    $x_password = pass_dec($dbPasshash, EMP_PASS);
+            $result1 = $this->conn->query($admSql1);
+
+            // ======== unsuccessfull admin registration process =========
+            if ($result1->num_rows == 0) {
+
+                $admSql2 = "SELECT * FROM `admin` WHERE (`email` = '$email' OR `username` = '$email') AND `reg_status`= 0";
+
+                $result2 = $this->conn->query($admSql2);
+
+
+                if ($result2->num_rows > 0) {
+                    $OTP = otpGgenerator();
+                    $timeout_duration = 600; // 10 * 60 seconds
+
+                    while ($admData2 = $result2->fetch_object()) {
+                        // print_r($admData2);
+
+                        $dbPasshash = $admData2->password;
+                        $x_password = pass_dec($dbPasshash, ADMIN_PASS);
+
+                        if ($x_password === $password) {
+
+                            session_start();
+
+                            $_SESSION['REGISTRATION']       = true;
+                            $_SESSION['ADMIN_REGISER']      = true;
+                            $_SESSION['PRIMARY_REGISTER']   = false;
+                            $_SESSION['SECONDARY_REGISTER'] = true;
+                            $_SESSION['session_start_time'] = date('H:i:s');
+                            $_SESSION['time_out']           = $timeout_duration;
+                            $_SESSION['verify_key']         = $OTP;
+                            $_SESSION['first-name']         = $admData2->fname;
+                            $_SESSION['email']              = $admData2->email;
+                            $_SESSION['username']           = $admData2->username;
+                            $_SESSION['adm_id']             = $admData2->admin_id;
+
+                            header("Location: register-mail.inc.php");
+                            exit;
+                        }
+                        return 'Wrong Password';
+                    }
+                }
+                header("Location: register.php");
+                exit;
+            }
+
+            // ======== admin login =========
+            if ($result1->num_rows > 0) {
+                while ($admData1 = $result1->fetch_object()) {
+
+                    $dbPasshash = $admData1->password;
+                    $x_password = pass_dec($dbPasshash, ADMIN_PASS);
+                    // exit;
 
                     if ($x_password === $password) {
 
                         session_start();
-                        $_SESSION['LOGGEDIN']       = true;
-                        $_SESSION['ADMIN']          = false;
-                        $_SESSION['USER_TYPE']      = 'USER';
-                        $_SESSION['EMP_EMAIL']      = $email;
-                        $_SESSION['EMP_CONTACT_NO'] = $data->emp_contact_no;
-                        $_SESSION['EMP_ROLE']       = $data->emp_role;
-                        $_SESSION['EMP_NAME']       = $data->emp_name;
-                        $_SESSION['EMP_IMG']        = $data->emp_img;
-                        $_SESSION['EMP_ADDRESS']    = $data->emp_address;
-                        $_SESSION['EMP_USERNAME']   = $data->emp_username;
-                        $_SESSION['EMP_PASSWORD']   = $data->emp_password;
-                        $_SESSION['EMPID']          = $data->emp_id;
-                        $_SESSION['ADMIN_ID']       = $data->admin_id;
+                        $_SESSION['LOGGEDIN']           = true;
+                        $_SESSION['ADMIN']              = true;
+                        $_SESSION['USER_TYPE']          = 'NOT USER';
+                        $_SESSION['ADMIN_EMAIL']        = $admData1->email;
+                        $_SESSION['USER_ROLE']          = 'ADMIN';
+                        $_SESSION['ADMIN_FNAME']        = $admData1->fname;
+                        $_SESSION['ADMIN_LNAME']        = $admData1->lname;
+                        $_SESSION['ADMIN_IMG']          = $admData1->adm_img;
+                        $_SESSION['ADMIN_CONTACT_NO']   = $admData1->mobile_no;
+                        $_SESSION['ADMIN_USERNAME']     = $admData1->username;
+                        $_SESSION['ADMIN_PASSWORD']     = $admData1->password;
+                        $_SESSION['ADMIN_ADDRESS']      = $admData1->address;
+                        $_SESSION['ADMINID']            = $admData1->admin_id;
+
+
+                        // Insert login time into login_time table
+                        // $adminId = $_SESSION['ADMINID'];
+                        // $loginTime = date('Y-m-d H:i:s');
+                        // $this->insertLoginTime($adminId, $loginTime);
+
 
                         header("Location: " . URL);
                         exit;
@@ -131,23 +132,21 @@ class LoginForm extends DatabaseConnection
                         return 'Wrong Password';
                     }
                 }
-            } else {
-                return 'not found';
             }
         }
     }
- 
+
 
     /// =======for login time =========///
-    function insertLoginTime($adminId, $loginTime)
-    {
-        $sql = "INSERT INTO login_time (id, login_time) VALUES ('$adminId', '$loginTime')";
-        $result = $this->conn->query($sql);
-        if ($result) {
-            return true;
-        } else {
-            error_log("Error inserting login time: " . $this->conn->error);
-            return false; 
-        }
-    }
+    // function insertLoginTime($adminId, $loginTime)
+    // {
+    //     $sql = "INSERT INTO login_time (id, login_time) VALUES ('$adminId', '$loginTime')";
+    //     $result = $this->conn->query($sql);
+    //     if ($result) {
+    //         return true;
+    //     } else {
+    //         error_log("Error inserting login time: " . $this->conn->error);
+    //         return false; 
+    //     }
+    // }
 }
