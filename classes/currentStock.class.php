486e1cbf105c782501a595a98bfca96e00fd218c
ROODRO <roodro.leelija@gmail.com>
2024-01-10 15:50:16 +0530

    update method.

--- classes/currentStock.class.php
+++ classes/currentStock.class.php
@@ -427,15 +427,14 @@ class CurrentStock extends DatabaseConnection
 
 
 
-
-    function showCurrentStockByPIdAndAdmin($productId='', $admin=''){
+    function showCurrentStockByPIdAndAdmin($productId, $admin){
         try {
             $data = array();
             $select = "SELECT * FROM current_stock WHERE `product_id` = ? AND `admin_id` = ? AND `qty` > '0' ORDER BY added_on ASC";
 
             $stmt = $this->conn->prepare($select);
 
-            $stmt->bind_param("si", $productId, $admin); 
+            $stmt->bind_param("ss", $productId, $admin); 
 
             $stmt->execute();
 
@@ -630,7 +629,7 @@ class CurrentStock extends DatabaseConnection
 
 
 
-    
+
 
     function checkStockExist($productId)
     {
