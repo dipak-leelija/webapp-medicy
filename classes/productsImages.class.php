554b2bac8b5d2704ebf5a350f761232acba3a48d
ROODRO <roodro.leelija@gmail.com>
2024-01-24 12:34:30 +0530

    modified code and updated.

--- classes/productsImages.class.php
+++ classes/productsImages.class.php
@@ -3,6 +3,33 @@
 class ProductImages extends DatabaseConnection
 {
 
+    function addImagesBySupAdmin($productId, $productImage, $status, $addedBy, $addedOn, $adminId){
+        try {
+            if (!empty($adminId)) {
+                $insertImage = "INSERT INTO `product_images` (`product_id`, `image`, `status`, `added_by`,  `added_on`, `admin_id`) VALUES (?, ?, ?, ?, ?, ?)";
+                $stmt = $this->conn->prepare($insertImage);
+                $stmt->bind_param("ssssss", $productId, $productImage, $status, $addedBy, $addedOn, $adminId);
+            } else {
+                $insertImage = "INSERT INTO `product_images` (`product_id`, `image`, `added_by`, `added_on`)    VALUES (?, ?, ?, ?)";
+                $stmt = $this->conn->prepare($insertImage);
+                $stmt->bind_param("ssss", $productId, $productImage, $addedBy, $addedOn);
+            }
+
+            if ($stmt->execute()) {
+                // Insert successful
+                $stmt->close();
+                return true;
+            } else {
+                // Insert failed
+                throw new Exception("Error inserting data into the database: " . $stmt->error);
+            }
+        } catch (Exception $e) {
+            return "Error: " . $e->getMessage();
+        }
+    }
+
+
+
 
 
     function addImages($productId, $productImage, $addedBy, $addedOn, $adminId)
