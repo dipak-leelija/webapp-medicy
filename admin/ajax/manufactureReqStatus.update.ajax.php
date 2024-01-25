f64aa2c7b5d0827d9ca888b8bbc767401f572c45
Jayanta Barman <jbleelija@gmail.com>
2024-01-25 11:07:36 +0530

    add this page for parchase master manufacturer request

---
+++ admin/ajax/manufactureReqStatus.update.ajax.php
@@ -0,0 +1,29 @@
+<?php
+require_once realpath(dirname(dirname(__DIR__)) . '/config/constant.php');
+require_once CLASS_DIR . 'dbconnect.php';
+require_once CLASS_DIR . 'manufacturer.class.php';
+
+$Manufacturer = new Manufacturer();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['manufacturerId']) && isset($_POST['newStatus'])) {
+    $manufacturerId = $_POST['manufacturerId'];
+    $newStatus = $_POST['newStatus'];
+
+    $updateManuStatus = $Manufacturer->updateManuStatus($newStatus, $manufacturerId);
+    if ($updateManuStatus) {
+
+        $deleteManufacturer = $Manufacturer->deleteRequestManufacturer($manufacturerId);
+        if ($deleteManufacturer) {
+            echo json_encode(['success' => true]);
+        } else {
+            echo json_encode(['success' => false, 'message' => 'Failed to delete Manufacturer request']);
+        }
+    } else {
+        echo json_encode(['success' => false, 'message' => 'Failed to update Manufacturer status']);
+    }
+    // if($updateManuStatus){
+    //     echo json_encode($updateManuStatus);
+    // }
+} else {
+    echo 'Invalid request';
+}
