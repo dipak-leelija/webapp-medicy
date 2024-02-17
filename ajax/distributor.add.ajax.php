74ddcae34a508afde94ffa4c985ad156886d796a
ROODRO <roodro.leelija@gmail.com>
2024-02-17 14:28:37 +0530

    all coment lines deleted.

--- ajax/distributor.add.ajax.php
+++ ajax/distributor.add.ajax.php
@@ -41,35 +41,6 @@ $Distributor = new Distributor();
         $addDistributor     = $Distributor->addDistributor(
             $distributorName, $distributorGSTID, $distributorAddress, $distributorAreaPIN, $distributorPhno, $distributorEmail, $distributorDsc, $employeeId, NOW,  $distributorStatus,$newData, $adminId
         );
-        print_r($addDistributor);
-
-//*************************************************************************************** */
-        // // Function to count how many times another function is called
-        // function countFunctionCalls($addDistributor)
-        // {
-        //     static $callCount = 0;
-
-        //     if (function_exists($addDistributor)) {
-        //         $callCount++;
-        //         call_user_func($addDistributor);
-        //     } else {
-        //         echo "Function '$addDistributor' does not exist.";
-        //     }
-
-        //     return $callCount;
-        // }
-
-        // // Function to be monitored
-        // function myFunction()
-        // {
-        //     echo "This is myFunction().<br>";
-        // }
-
-        // // Call countFunctionCalls() to invoke myFunction()
-        // $count = countFunctionCalls('myFunction');
-        // echo "myFunction() has been called $count times.<br>";
-
-//******************************************************************************************** */
        
 
         if ($addDistributor == true) {
