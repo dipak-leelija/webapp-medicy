7289493b7ae89dee626d6afc13a19cca2aa051bd
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2024-02-23 18:12:52 +0530

    purchase return page updated and current stock item modal set to xl

--- stock-return.php
+++ stock-return.php
@@ -47,11 +47,13 @@ $empLists              = $Employees->employeesDisplay($adminId);
     <!-- Custom fonts for this template -->
     <link href="<?= PLUGIN_PATH ?>fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 
 
     <!-- Custom styles for this template -->
-    <link href="<?= CSS_PATH ?>sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?= CSS_PATH ?>sb-admin-2.css" rel="stylesheet">
     <link rel="stylesheet" href="<?= CSS_PATH ?>custom/return-page.css">
 
     <!-- Datatable Style CSS -->
@@ -88,17 +90,21 @@ $empLists              = $Employees->employeesDisplay($adminId);
                     <div class="d-flex justify-content-between">
 
                     </div>
-                    <!-- Add Product -->
+                    <!-- Main Card Start -->
                     <div class="card shadow mb-2">
                         <div class="card-body">
+                            <?php 
+                            if($stockReturnLists->status){ 
+                            $stockReturnLists = $stockReturnLists->data ; 
+                        ?>
                             <div class="row mb-3 ">
                                 <div class="col-md-2 col-12">
                                     <label for="">Filter By:</label>
-
                                 </div>
 
                                 <div class="col-md-3 col-12">
-                                    <select class="cvx-inp1" name="added_on" id="added_on" onchange="returnFilter(this)">
+                                    <select class="cvx-inp1" name="added_on" id="added_on"
+                                        onchange="returnFilter(this)">
                                         <option value="" disabled selected>Select Duration</option>
                                         <option value="T">Today</option>
                                         <option value="Y">yesterday</option>
@@ -123,10 +129,13 @@ $empLists              = $Employees->employeesDisplay($adminId);
                                     </select>
                                 </div>
                                 <div class="col-md-2 col-6">
-                                    <input class="cvx-inp" type="text" placeholder="Distributor Name" name="distributor_id" id="distributor_id" style="outline: none;" onkeyup="returnFilter(this)">
+                                    <input class="cvx-inp" type="text" placeholder="Distributor Name"
+                                        name="distributor_id" id="distributor_id" style="outline: none;"
+                                        onkeyup="returnFilter(this)">
                                 </div>
                                 <div class="col-md-2 col-6">
-                                    <select class="cvx-inp1" name="refund_mode" id="refund_mode" onchange="returnFilter(this)">
+                                    <select class="cvx-inp1" name="refund_mode" id="refund_mode"
+                                        onchange="returnFilter(this)">
                                         <option value="" selected disabled>payment Mode</option>
                                         <option value="Credit">Credit</option>
                                         <option value="Cash">Cash</option>
@@ -139,7 +148,8 @@ $empLists              = $Employees->employeesDisplay($adminId);
                                     </select>
                                 </div>
                                 <div class="col-md-1 col-6 text-right">
-                                    <a class="btn btn-sm btn-primary " href="stock-return-item.php"> New <i class="fas fa-plus"></i></a>
+                                    <a class="btn btn-sm btn-primary " href="stock-return-item.php"> New <i
+                                            class="fas fa-plus"></i></a>
                                 </div>
                             </div>
                             <!-- ============================= date picker div ================================== -->
@@ -150,17 +160,16 @@ $empLists              = $Employees->employeesDisplay($adminId);
                                     <input type="date" id="from-date" name="from-date">
                                     <label for="end-date">To Date:</label>
                                     <input type="date" id="to-date" name="to-date">
-                                    <button class="btn btn-sm btn-primary" id="added_on" value="CR" onclick="getDates(this.id, this.value)" style="height: 2rem;">Find</button>
+                                    <button class="btn btn-sm btn-primary" id="added_on" value="CR"
+                                        onclick="getDates(this.id, this.value)" style="height: 2rem;">Find</button>
                                 </div>
                             </div>
 
                             <!-- ================== eof date picker div ======================== -->
 
                             <div class="table-responsive" id="filter-table">
-                                <?php 
-                                    if($stockReturnLists->status){ 
-                                    $stockReturnLists = $stockReturnLists->data ; 
-                                ?>
+
+                                <!-- table start -->
                                 <table class="table table-sm table-hover" id="<?php if (count($stockReturnLists) > 10) {
                                                                                     echo "dataTable";
                                                                                 } ?>" width="100%" cellspacing="0">
@@ -218,14 +227,63 @@ $empLists              = $Employees->employeesDisplay($adminId);
                                         ?>
                                     </tbody>
                                 </table>
-                                <?php }else{ ?>
-                                    <tr>
-                                            <th>No data Available</th>
-                                    </tr>
-                                    <?php } ?>
+                                <!-- table end -->
+
                             </div>
+                            <?php }else{ ?>
+                            <div class="container p-3">
+                                <div class="row justify-content-center">
+                                    <div class="card-body">
+                                        <div class="m-auto" style="width: 15%;">
+                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1"
+                                                x="0px" y="0px" viewBox="0 0 40 40"
+                                                style="enable-background:new 0 0 40 40;" xml:space="preserve">
+                                                <g>
+                                                    <g>
+                                                        <path style="fill:#98CCFD;"
+                                                            d="M24.405,20.771c0.646-1.188,0.949-2.402,0.906-3.631c-0.101-2.912-2.115-5.258-3.787-6.713    l-0.352-0.307L18.5,12.793V4.5h8.293l-2.32,2.32l0.379,0.353c1.6,1.493,3.506,3.803,3.506,6.327    C28.357,16.728,26.229,19.224,24.405,20.771z" />
+                                                    </g>
+                                                    <g>
+                                                        <path style="fill:#4788C7;"
+                                                            d="M25.586,5l-1.1,1.1l-0.732,0.732l0.756,0.706c1.25,1.167,3.347,3.493,3.347,5.962    c0,2.132-0.997,3.935-2.195,5.332c0.119-0.565,0.169-1.136,0.149-1.71c-0.107-3.092-2.212-5.553-3.959-7.073l-0.704-0.612    l-0.66,0.66L19,11.586V5H25.586 M28,4H18v10l3.196-3.196c2.537,2.208,6.227,6.844,0.875,12.196c0,0,6.786-3.5,6.786-9.5    c0-2.749-2.079-5.213-3.664-6.693L28,4L28,4z" />
+                                                    </g>
+                                                </g>
+                                                <path
+                                                    style="fill:none;stroke:#4788C7;stroke-linecap:round;stroke-miterlimit:10;"
+                                                    d="M37.368,11.682L33.9,24.395  c-0.177,0.65-0.772,1.105-1.447,1.105H11.911" />
+                                                <g>
+                                                    <circle style="fill:#DFF0FE;" cx="2" cy="3" r="1.5" />
+                                                    <path style="fill:#4788C7;"
+                                                        d="M2,2c0.551,0,1,0.449,1,1S2.551,4,2,4S1,3.551,1,3S1.449,2,2,2 M2,1C0.895,1,0,1.895,0,3   s0.895,2,2,2s2-0.895,2-2S3.105,1,2,1L2,1z" />
+                                                </g>
+                                                <g>
+                                                    <circle style="fill:#98CCFD;" cx="30.5" cy="36.5" r="2" />
+                                                    <path style="fill:#4788C7;"
+                                                        d="M30.5,35c0.827,0,1.5,0.673,1.5,1.5S31.327,38,30.5,38S29,37.327,29,36.5S29.673,35,30.5,35    M30.5,34c-1.381,0-2.5,1.119-2.5,2.5s1.119,2.5,2.5,2.5s2.5-1.119,2.5-2.5S31.881,34,30.5,34L30.5,34z" />
+                                                </g>
+                                                <g>
+                                                    <circle style="fill:#98CCFD;" cx="17.5" cy="36.5" r="2" />
+                                                    <path style="fill:#4788C7;"
+                                                        d="M17.5,35c0.827,0,1.5,0.673,1.5,1.5S18.327,38,17.5,38S16,37.327,16,36.5S16.673,35,17.5,35    M17.5,34c-1.381,0-2.5,1.119-2.5,2.5s1.119,2.5,2.5,2.5s2.5-1.119,2.5-2.5S18.881,34,17.5,34L17.5,34z" />
+                                                </g>
+                                                <path
+                                                    style="fill:none;stroke:#4788C7;stroke-linecap:round;stroke-miterlimit:10;"
+                                                    d="M2,1.5h2.119  c2.093,0,3.882,1.505,4.241,3.567l4.498,25.866c0.359,2.062,2.148,3.567,4.241,3.567H34.5" />
+                                            </svg>
+                                        </div>
+                                        <h5 class="card-title text-center py-2">Return not Initiated yet!</h5>
+                                        <div class="d-flex justify-content-cenetr justify-content-center">
+                                            <a class="btn btn-primary" href="stock-return-item.php">Initiate Return</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <?php } ?>
                         </div>
                     </div>
+                    <!-- Main Card End -->
 
                     <!--=========================== Show Bill Items ===========================-->
 
@@ -242,7 +300,8 @@ $empLists              = $Employees->employeesDisplay($adminId);
 
             <!-- go to stock-return-control.js for function contorl -->
             <!-- Return View Modal" -->
-            <div class="modal fade" id="viewReturnModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id="viewReturnModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
                 <div class="modal-dialog modal-xl modal-dialog-centered">
                     <div class="modal-content">
                         <div class="modal-header">
