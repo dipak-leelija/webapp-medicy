4fd058bd0db28b24a05b75f00256332a23f8d9cf
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2024-02-23 17:37:31 +0530

    sb-admin-2.min.css removed and sb-admin-2.css used

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ $labAppointment     = $LabAppointments->showLabAppointmentsByAdminId($adminId);
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="<?php echo CSS_PATH; ?>sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?php echo CSS_PATH; ?>sb-admin-2.css" rel="stylesheet">
     <link href="<?php echo CSS_PATH; ?>custom/custom.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo CSS_PATH; ?>custom-dashboard.css">
 
