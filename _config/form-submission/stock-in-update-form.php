5ae5a11f838484b38b420ee17107c993b4a0c8be
ROODRO <roodro.leelija@gmail.com>
2024-03-05 12:46:51 +0530

    organization name and logo path modified for stock in edit bill.

--- _config/form-submission/stock-in-update-form.php
+++ _config/form-submission/stock-in-update-form.php
@@ -13,6 +13,8 @@ require_once CLASS_DIR . 'packagingUnit.class.php';
 require_once CLASS_DIR . 'stockOut.class.php';
 require_once CLASS_DIR . 'stockReturn.class.php';
 require_once CLASS_DIR . 'salesReturn.class.php';
+require_once CLASS_DIR . 'hospital.class.php';
+
 
 $StockIn = new StockIn();
 $StockInDetails = new StockInDetails();
@@ -25,6 +27,7 @@ $PackagingUnits = new PackagingUnits();
 $StockOut = new StockOut();
 $StcokReturn = new StockReturn();
 $SalesReturn = new SalesReturn();
+$ClinicInfo  = new HealthCare;
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -342,6 +345,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
+$selectClinicInfo = json_decode($ClinicInfo->showHealthCare($adminId));
+// print_r($selectClinicInfo->data);
+$pharmacyLogo = $selectClinicInfo->data->logo;
+$pharmacyName = $selectClinicInfo->data->hospital_name;
+// echo $pharmacyLogo;
 ?>
 
 <!DOCTYPE html>
@@ -379,7 +387,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="card-body ">
                 <div class="row">
                     <div class="col-sm-1">
-                        <img class="float-end" style="height: 55px; width: 58px;" src="<?= SITE_IMG_PATH ?>logo-p.jpg" alt="Medicy">
+                        <img class="float-end" style="height: 55px; width: 58px;" src="<?= LOCAL_DIR.$pharmacyLogo ?>" alt="Medicy">
+                        <div><?php echo $pharmacyName ?></div>
                     </div>
                     <div class="col-sm-8">
                         <h4 class="text-start my-0"><?php echo $distributorName; ?></h4>
