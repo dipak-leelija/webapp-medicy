2baab1ff5d39dd2bbc748c7b48ee74c81b40a290
ROODRO <roodro.leelija@gmail.com>
2024-03-07 18:30:50 +0530

    create new file for doc specialization data add through ajax call. modified window alert with sweet alert.

--- doctor-specialization.php
+++ doctor-specialization.php
@@ -1,28 +1,28 @@
 <?php
 $page = "doc-specialization";
-require_once __DIR__.'/config/constant.php';
-require_once ROOT_DIR.'_config/sessionCheck.php';
+require_once __DIR__ . '/config/constant.php';
+require_once ROOT_DIR . '_config/sessionCheck.php';
 
 
-require_once CLASS_DIR.'dbconnect.php';
-require_once ROOT_DIR.'_config/healthcare.inc.php';
-require_once CLASS_DIR.'doctor.category.class.php';
+require_once CLASS_DIR . 'dbconnect.php';
+require_once ROOT_DIR . '_config/healthcare.inc.php';
+require_once CLASS_DIR . 'doctor.category.class.php';
 
 $nodataFound = 'NO DATA FOUND';
 // INITILIZATION CLASSES
 $DoctorCategory = new DoctorCategory();
 
-if (isset($_POST['add'])) {
+/*if (isset($_POST['add'])) {
     $docCatNme = $_POST['splz-name'];
     $docDesc   = $_POST['splz-dsc'];
 
     $addDoctorCategory = $DoctorCategory->addDoctorCategory($docCatNme, $docDesc, $employeeId, NOW, $adminId);
-    if ($addDoctorCategory) {
-        echo '<script> alert("Doctor Specialization Added!");</script>';
-    }else {
-        echo '<script> alert("Failed!");</script>';
-    }
-}
+    // if ($addDoctorCategory) {
+    //     echo '<script> alert("Doctor Specialization Added!");</script>';
+    // }else {
+    //     echo '<script> alert("Failed!");</script>';
+    // }
+}*/
 
 
 // FUNCTION INITILIZATION 
@@ -52,6 +52,7 @@ $showDoctorCategory = $DoctorCategory->showDoctorCategoryByAdmin($adminId);
 
     <!-- Custom styles for this template-->
     <link href="<?php echo CSS_PATH ?>sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?php echo CSS_PATH ?>sweetalert2/sweetalert2.min.css" rel="stylesheet">
 
     <!-- <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet"> -->
 
@@ -64,7 +65,7 @@ $showDoctorCategory = $DoctorCategory->showDoctorCategoryByAdmin($adminId);
     <div id="wrapper">
 
         <!-- sidebar -->
-        <?php include ROOT_COMPONENT.'sidebar.php'; ?>
+        <?php include ROOT_COMPONENT . 'sidebar.php'; ?>
         <!-- end sidebar -->
 
         <!-- Content Wrapper -->
@@ -74,7 +75,7 @@ $showDoctorCategory = $DoctorCategory->showDoctorCategoryByAdmin($adminId);
             <div id="content">
 
                 <!-- Topbar -->
-                <?php include ROOT_COMPONENT.'topbar.php'; ?>
+                <?php include ROOT_COMPONENT . 'topbar.php'; ?>
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
@@ -86,22 +87,22 @@ $showDoctorCategory = $DoctorCategory->showDoctorCategoryByAdmin($adminId);
                         <div class="col-md-5">
                             <div class="card shadow mb-4">
                                 <div class="card-body">
-                                    <form method="post">
+                                    <!-- <form method="post"> -->
 
-                                        <div class="col-md-12">
-                                            <label class="mb-0 mt-1" for="splz-name">Specialization<span class="text-danger font-weight-bold">*</span></Address></label>
-                                            <input class="form-control" id="splz-name" name="splz-name" placeholder="Doctor Specialization Name" required>
-                                        </div>
+                                    <div class="col-md-12">
+                                        <label class="mb-0 mt-1" for="splz-name">Specialization<span class="text-danger font-weight-bold">*</span></Address></label>
+                                        <input class="form-control" id="splz-name" name="splz-name" placeholder="Doctor Specialization Name" required>
+                                    </div>
 
-                                        <div class="col-md-12 mt-3">
-                                            <label class="mb-0 mt-1" for="splz-dsc">Description</Address></label>
-                                            <textarea class="form-control" name="splz-dsc" id="splz-dsc" cols="30" rows="4"></textarea>
-                                        </div>
+                                    <div class="col-md-12 mt-3">
+                                        <label class="mb-0 mt-1" for="splz-dsc">Description</Address></label>
+                                        <textarea class="form-control" name="splz-dsc" id="splz-dsc" cols="30" rows="4" required></textarea>
+                                    </div>
 
-                                        <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-3 me-md-2">
-                                            <button class="btn btn-primary me-md-2" name="add" type="submit">Add</button>
-                                        </div>
-                                    </form>
+                                    <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-3 me-md-2">
+                                        <button class="btn btn-primary me-md-2 add-doctor-specialization" id="add-btn" name="add" type="submit">Add</button>
+                                    </div>
+                                    <!-- </form> -->
                                 </div>
                             </div>
                         </div>
@@ -121,30 +122,29 @@ $showDoctorCategory = $DoctorCategory->showDoctorCategoryByAdmin($adminId);
                                                 </tr>
                                             </thead>
                                             <tbody>
-                                            <?php
-                                            if(is_array($showDoctorCategory) && $showDoctorCategory != null){
-                                                foreach ($showDoctorCategory as $rowDoctorCategory) {
-                                                    $specializationid = $rowDoctorCategory['doctor_category_id'];
-                                                    $specializationName = $rowDoctorCategory['category_name'];
-                                                    $specializationDsc = $rowDoctorCategory['category_descreption'];
+                                                <?php
+                                                if (is_array($showDoctorCategory) && $showDoctorCategory != null) {
+                                                    foreach ($showDoctorCategory as $rowDoctorCategory) {
+                                                        $specializationid = $rowDoctorCategory['doctor_category_id'];
+                                                        $specializationName = $rowDoctorCategory['category_name'];
+                                                        $specializationDsc = $rowDoctorCategory['category_descreption'];
 
-                                                    echo '<tr>
-                                                    <td>'.$specializationid.'</td>
-                                                    <td>'.$specializationName.'</td>
-                                                    <td>'.substr($specializationDsc, 0, 65).'..</td>
+                                                        echo '<tr>
+                                                    <td>' . $specializationid . '</td>
+                                                    <td>' . $specializationName . '</td>
+                                                    <td>' . substr($specializationDsc, 0, 65) . '..</td>
                                                     <td>
                                                         <a class="text-primary" data-toggle="modal" data-target=".docCatModal"
-                                                            onclick="viewAndEdit('.$specializationid.')"><i
+                                                            onclick="viewAndEdit(' . $specializationid . ')"><i
                                                                 class="fas fa-edit"></i></a>
 
-                                                        <a class="text-primary delete-btn" data-id="'.$specializationid.'"><i class="far fa-trash-alt"></i></a>
+                                                        <a class="text-primary delete-btn" data-id="' . $specializationid . '"><i class="far fa-trash-alt"></i></a>
                                                     </td>
                                                 </tr>';
-
+                                                    }
                                                 }
-                                            }
-                                            ?>
-                                                
+                                                ?>
+
                                             </tbody>
                                         </table>
                                     </div>
@@ -160,7 +160,7 @@ $showDoctorCategory = $DoctorCategory->showDoctorCategoryByAdmin($adminId);
             <!-- End of Main Content -->
 
             <!-- Footer -->
-            <?php include ROOT_COMPONENT.'footer-text.php'; ?>
+            <?php include ROOT_COMPONENT . 'footer-text.php'; ?>
             <!-- End of Footer -->
 
         </div>
@@ -175,8 +175,7 @@ $showDoctorCategory = $DoctorCategory->showDoctorCategoryByAdmin($adminId);
     </a>
 
     <!-- View & Edit Doctor Category Modal -->
-    <div class="modal fade docCatModal" tabindex="-1" role="dialog"
-        aria-labelledby="docCatModalLabel" aria-hidden="true">
+    <div class="modal fade docCatModal" tabindex="-1" role="dialog" aria-labelledby="docCatModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
@@ -217,48 +216,100 @@ $showDoctorCategory = $DoctorCategory->showDoctorCategoryByAdmin($adminId);
 
     <!-- Page level custom scripts -->
     <script src="<?php echo JS_PATH ?>demo/datatables-demo.js"></script>
+    <script src="<?php echo JS_PATH ?>sweetalert2/sweetalert2.all.min.js"></script>
+
+
 
     <script>
+        // add doctor specialization 
+        $(document).ready(function() {
+            $(document).on("click", ".add-doctor-specialization", function() {
+                let docSpecialization = document.getElementById('splz-name').value;
+                let docSpecializationDescription = document.getElementById('splz-dsc').value;
+
+                if(docSpecialization != '' && docSpecializationDescription != ''){
+                    console.log(docSpecialization);
+                    console.log(docSpecializationDescription);
+
+                    // ajax/doc-specialization-add.ajax.php
+                    $.ajax({
+                            url: "ajax/doc-specialization-add.ajax.php",
+                            type: "POST",
+                            data: {
+                                docSpeclzn: docSpecialization,
+                                docSpeclznDsc: docSpecializationDescription
+                            },
+                            success: function(response) {
+                                // alert(response);
+                                if (response) {
+                                    // Swal.fire('Success', 'Data added successfully', 'success');
+                                    window.location.reload();
+                                } else {
+                                    // Swal.fire('Fail', 'Fail to add data.', 'error');
+                                    window.location.reload();
+                                }
+                            }
+                        });
+
+                }
+                
+                document.getElementById('splz-name').value = '';
+                document.getElementById('splz-dsc').value = '';
+            })
+        })
+
+
         //View and Update Doctor Specialization
-        viewAndEdit = (specializationid) =>{
+        viewAndEdit = (specializationid) => {
             let url = "ajax/docCatView.ajax.php?specializationid=" + specializationid;
             $(".view-and-edit-specialization").html(
-            '<iframe width="99%" height="340px" frameborder="0" allowtransparency="true" src="' +
-            url + '"></iframe>');
+                '<iframe width="99%" height="340px" frameborder="0" allowtransparency="true" src="' +
+                url + '"></iframe>');
         }
 
 
         // Delete Doctor Specialization
         $(document).ready(function() {
-        $(document).on("click", ".delete-btn", function() {
+            $(document).on("click", ".delete-btn", function() {
 
-            if (confirm("Are You Sure?")) {
-                CatId = $(this).data("id");
-                // alert(CatId);
-                btn = this;
+                Swal.fire({
+                    title: "Are you sure?",
+                    text: "You won't be able to revert this!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Yes, delete it!"
 
-                $.ajax({
-                    url: "ajax/docCat.delete.ajax.php", 
-                    type: "POST",
-                    data: {
-                        id: CatId
-                    },
-                    success: function (response) {
-                        if (response == 1) {
-                            $(btn).closest("tr").fadeOut()
-                        } else {
-                        alert("Deletion Failed!");
-                            $("#error-message").html("Deletion Failed !!!").slideDown();
-                            $("success-message").slideUp();
-                        }
+                }).then((result) => {
+                    if (result.isConfirmed) {
 
+                        CatId = $(this).data("id");
+                        // alert(CatId);
+                        btn = this;
+
+                        $.ajax({
+                            url: "ajax/docCat.delete.ajax.php",
+                            type: "POST",
+                            data: {
+                                id: CatId
+                            },
+                            success: function(response) {
+                                if (response == 1) {
+                                    $(btn).closest("tr").fadeOut()
+                                } else {
+                                    alert("Deletion Failed!");
+                                    $("#error-message").html("Deletion Failed !!!").slideDown();
+                                    $("success-message").slideUp();
+                                }
+                            }
+                        });
                     }
                 });
-            }
-            return false;
-        })
 
-    })
+            })
+
+        })
     </script>
 
 
