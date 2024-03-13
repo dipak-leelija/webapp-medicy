6cf4ac572518ab9bcb34b7b8f8aa7fad7b52af4d
ROODRO <roodro.leelija@gmail.com>
2024-03-13 18:23:29 +0530

    create new function for verification and validation of contact and email.

--- assets/js/add-patient.js
+++ assets/js/add-patient.js
@@ -6,29 +6,64 @@ const checkMail = (t) =>{
 
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if(emailRegex.test(email)){
-        document.getElementById('patientPhoneNumber').focus();
+        checkMailExistance(email);
     }else{
         t.value = '';
         Swal.fire('Alert','Enter valid email id.','info');
-        t.focus();
     }
 }
 
+// check mail existance
+const checkMailExistance = (email) =>{
+    let checkEmail = email;
+    $.ajax({
+        url: 'ajax/mobile-email-existance-check.ajax.php',
+        type: 'POST',
+        data: {
+            email: checkEmail,
+        },
+        success: function (data) {
+            if (data == 1) {
+                // alert("email check"+data);
+                Swal.fire('Alert','Email exist.','error');
+                // document.getElementById('patientEmail').focus();
+                // document.getElementById('patientEmail').value = '';
+            } else {
+                document.getElementById('patientPhoneNumber').focus();
+            }
+        },
+    });
+}
 
-//// check inputed mobile length validity
-const checkMobNo = (t) =>{
-    if(t.value.length > 9){
-        Swal.fire('Alert','Enter Maximum 10 digit','error');
+
+
+// mobile verification and validation =======
+const checkContactNo = (t) => {
+    if (t.value.length != 10) {
+        Swal.fire('Alert', 'Mobile number must be 10 digits', 'error');
         t.value = '';
+    } else {
+        let contactNo = t.value;
+        $.ajax({
+            url: 'ajax/mobile-email-existance-check.ajax.php',
+            type: 'POST',
+            data: {
+                checkMobNo: contactNo,
+            },
+            success: function (data) {
+                // alert(data);
+                if (data == 1) {
+                    Swal.fire('Alert', 'Mobile number already exists.', 'error');
+                } else {
+                    document.getElementById("appointmentDate").focus();
+                }
+            },
+        });
     }
 }
 
-const checkContactNo = (t) =>{
-    if(t.value.length < 9){
-        Swal.fire('Error','Mobile number must be 10 digit','error');
-        t.value = '';
-    }
-}
+
+
 
 // patient weight validity
 const checkWeight = (t) =>{
