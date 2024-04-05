bc1b4c1c70ed670a19b331000eba3a2b15523d05
ROODRO <roodro.leelija@gmail.com>
2024-04-05 15:41:26 +0530

    reset button and sign modified.

--- stock-in.php
+++ stock-in.php
@@ -69,6 +69,7 @@ $todayYr = date("y");
     <!-- Custom fonts for this template -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="<?= PLUGIN_PATH ?>fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= PLUGIN_PATH ?>font-asesome-5/font-awesome-5.15.4-all.min.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="<?= CSS_PATH ?>custom/stock-in.css">
 
     <!-- Custom styles for this template -->
