88347e79b3cd9cd9978ff839c573c0d4e182bb7b
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2024-04-08 14:34:32 +0530

    purchase details view updated

--- purchase-details.php
+++ purchase-details.php
@@ -175,17 +175,13 @@ $showDistributor       = $Distributor->showDistributor();
                 <div class="modal-dialog modal-xl modal-dialog-scrollable">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Stock-in Details</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Purchase Details</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body stockDetails">
                         </div>
-                        <!-- <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div> -->
                     </div>
                 </div>
             </div>
