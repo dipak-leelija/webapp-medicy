fa8cf7ee75c92fdd8fa0adc009f2e53e84fbb530
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2024-05-23 16:49:31 +0530

    local origin added

--- api/headers.php
+++ api/headers.php
@@ -1,6 +1,6 @@
 <?php
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-$allowed_origins = ['https://medicy.in/', 'https://medicy.in', 'https://leelija.com'];
+$allowed_origins = ['https://medicy.in/', 'https://medicy.in', 'http://localhost'];
 
 // $origin = '*';
 // $allowed_origins = ['*'];
