c647655db0d0be622d140f9480887acf972fff06
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2024-05-23 16:42:28 +0530

    header cheacking

--- api/headers.php
+++ api/headers.php
@@ -1,6 +1,6 @@
 <?php
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-$allowed_origins = ['https://leelija.com'];
+$allowed_origins = ['https://medicy.in/', 'https://medicy.in', 'https://leelija.com'];
 
 // $origin = '*';
 // $allowed_origins = ['*'];
