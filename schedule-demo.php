759d3052da4bbaff1d266b41843ebc86924fcdf8
ROODRO <roodro.leelija@gmail.com>
2024-05-23 10:54:54 +0530

    creating schedule-demo.php page for tesing calendly link.

---
+++ schedule-demo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Schedule - Demo</title>
+</head>
+
+<body>
+    <div class="container">
+        <!-- Calendly inline widget begin -->
+        <div class="calendly-inline-widget" data-url="https://calendly.com/roodro-leelija/product-demo" style="min-width:320px;height:700px;"></div>
+        <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+        <!-- Calendly inline widget end -->
+    </div>
+</body>
+
+</html>
