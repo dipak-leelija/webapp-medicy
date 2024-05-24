ccddc29fcbf5f9a5362d3214726e57d7b71c11aa
ROODRO <roodro.leelija@gmail.com>
2024-05-24 18:14:41 +0530

    added contact and email validation in server side.

--- api/controllers/ContactController.php
+++ api/controllers/ContactController.php
@@ -11,8 +11,7 @@ class ApiContactController {
     public function createContact($data) {
         $contactModel = new Contact();
         $result = json_decode($contactModel->createContact($data));
-        print_r($result);
-        
+        // print_r($result);
         header("Content-Type: application/json; charset=UTF-8");
 
         if($result->status){
