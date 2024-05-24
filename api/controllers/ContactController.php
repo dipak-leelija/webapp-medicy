582c3e3b63d6852f8082f0ca9bf20b96251a53c9
ROODRO <roodro.leelija@gmail.com>
2024-05-24 18:09:46 +0530

    checking error.

--- api/controllers/ContactController.php
+++ api/controllers/ContactController.php
@@ -11,7 +11,8 @@ class ApiContactController {
     public function createContact($data) {
         $contactModel = new Contact();
         $result = json_decode($contactModel->createContact($data));
-
+        print_r($result);
+        
         header("Content-Type: application/json; charset=UTF-8");
 
         if($result->status){
