de5ad366e4a1dffe3f573a06e45141346eeb4776
ROODRO <roodro.leelija@gmail.com>
2024-05-24 18:05:43 +0530

    add contact and mail validation in serverside.

--- api/models/ContactModel.php
+++ api/models/ContactModel.php
@@ -7,46 +7,57 @@ require_once dirname(__DIR__, 2) . '/classes/dbconnection.php';
 use DatabaseConnection\DatabaseConnection;
 use Exception;
 
-class Contact {
+class Contact
+{
     private $conn;
 
-    public function __construct() {
+    public function __construct()
+    {
         $db = new DatabaseConnection();
         $this->conn = $db->conn;
     }
 
-    public function createContact($data) {
+    public function createContact($data)
+    {
         try {
-            
+
             if (!empty($data['name']) && !empty($data['contact_number']) && !empty($data['email']) && !empty($data['subject']) && !empty($data['message'])) {
-                
-                $query = "INSERT INTO contact_details (name, contact_number, email, subject, message) 
+
+                if (strlen($data['contact_number']) == 10) {
+
+                    if (filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+
+                        $query = "INSERT INTO contact_details (name, contact_number, email, subject, message) 
                           VALUES (?, ?, ?, ?, ?)";
-                
-                if ($stmt = $this->conn->prepare($query)) {
 
-                    $stmt->bind_param('sisss', $data['name'], $data['contact_number'], $data['email'], $data['subject'], $data['message']);
+                        if ($stmt = $this->conn->prepare($query)) {
+
+                            $stmt->bind_param('sisss', $data['name'], $data['contact_number'], $data['email'], $data['subject'], $data['message']);
 
 
-                    if ($stmt->execute()) {
-                        $insertId = $stmt->insert_id;
-                        return json_encode(['status' => true, 'insertId' => $insertId, 'message'=>'Data stored successfully']);
+                            if ($stmt->execute()) {
+                                $insertId = $stmt->insert_id;
+                                return json_encode(['status' => true, 'insertId' => $insertId, 'message' => 'Thank You, We will get in touch soon!']);
+                            } else {
+                                // error_log("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
+                                return json_encode(['status' => false, 'message' => 'Code Executin failed: ' . $stmt->error]);
+                            }
+                        } else {
+                            // error_log("Prepare failed: (" . $this->conn->errno . ") " . $this->conn->error);
+                            return json_encode(['status' => false, 'message' => 'Data Preparation failed: ' . $this->conn->error]);
+                        }
                     } else {
-                        // error_log("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
-                        return json_encode(['status' => false, 'message' => 'Code Executin failed: ' . $stmt->error]);
+                        return json_encode(['status' => false, 'message' => 'Enter valid email id']);
                     }
-
                 } else {
-                    // error_log("Prepare failed: (" . $this->conn->errno . ") " . $this->conn->error);
-                    return json_encode(['status' => false, 'message' => 'Data Preparation failed: ' . $this->conn->error]);
+                    return json_encode(['status' => false, 'message' => 'Enter valid contact number']);
                 }
             } else {
-                return json_encode(['status' => false, 'message' => 'Blank data input']);
+                return json_encode(['status' => false, 'message' => 'Please fill all required fields!']);
             }
         } catch (Exception $e) {
             // error_log("Exception: " . $e->getMessage());
             return json_encode(['status' => false, 'message' => 'Exception: ' . $e->getMessage()]);
         }
     }
-
 }
