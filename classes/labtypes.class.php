a50926597b24e2cc0a3f9cb69c00f9d9767e8fb0
ROODRO <roodro.leelija@gmail.com>
2024-05-29 18:09:48 +0530

    lab test data search funcion workign.

--- classes/labtypes.class.php
+++ classes/labtypes.class.php
@@ -1,36 +1,38 @@
 <?php
 
-class LabTypes extends UtilityFiles{
-    
-    use DatabaseConnection;
-    
+class LabTypes extends UtilityFiles
+{
 
-    function addLabTypes($image, $testName, $testPvdBy, $testDsc){
+    use DatabaseConnection;
+
+
+    function addLabTypes($image, $testName, $testPvdBy, $testDsc)
+    {
         try {
             $addLabType = "INSERT INTO `tests_types` (`image`, `test_type_name`, `provided_by`, `dsc`) VALUES (?, ?, ?, ?)";
             $stmt = $this->conn->prepare($addLabType);
-    
+
             if ($stmt === false) {
                 throw new Exception('Prepare failed: ' . $this->conn->error);
             }
-    
+
             $stmt->bind_param('ssss', $image, $testName, $testPvdBy, $testDsc);
-    
+
             $result = $stmt->execute();
-    
+
             if ($stmt->affected_rows > 0) {
                 $response = ['status' => true, 'message' => 'success'];
             } else {
                 $response = ['status' => false, 'message' => 'Data insertion fails'];
             }
-    
+
             $stmt->close();
             return json_encode($response);
         } catch (Exception $e) {
             return json_encode(['status' => false, 'message' => 'Error: ' . $e->getMessage()]);
         }
     }
-    
+
 
 
 
@@ -44,19 +46,49 @@ class LabTypes extends UtilityFiles{
             $labTypeQuery = $this->conn->query($selectLabType);
             $rows = $labTypeQuery->num_rows;
             if ($rows == 0) {
-                return 0;
+                $data = [];
             } else {
                 while ($result = $labTypeQuery->fetch_array()) {
                     $data[] = $result;
                 }
-                return $data;
-                // return json_encode(['status' => 1, 'message' => 'success', 'data' => $data]);
+                return json_encode(['status' => 1, 'message' => 'success', 'data' => $data]);
             }
         } catch (Exception $e) {
             $e->getMessage();
-            // return json_encode(['status' => 0, 'message' => '', 'data' => '']);
         }
     } // end showLabTypes function
+
+
+
+
+    function searchLabTest($search)
+    {
+        try {
+            $data = [];
+            $searchLabTestData = "SELECT * FROM `tests_types` WHERE `test_type_name` LIKE '%$search%'";
+            $stmt = $this->conn->prepare($searchLabTestData);
+
+            if ($stmt) {
+                $stmt->execute();
+                $result = $stmt->get_result();
+
+                if ($result->num_rows > 0) {
+                    while ($resultData = $result->fetch_array()) {
+                        $data[] = $resultData;
+                    }
+                }
+                $stmt->close();
+                return json_encode(['status' => 1, 'data' => $data]);
+            } else {
+                throw new Exception("Failed to prepare statement");
+            }
+        } catch (Exception $e) {
+            return json_encode(['status' => 0, 'message' => $e->getMessage()]);
+        }
+    } // end searchLabTest function
+
+
+
 
 
     function showLabTypesById($showLabtypeId)
@@ -96,42 +128,43 @@ class LabTypes extends UtilityFiles{
     //     return $deletelabQuery;
     // } // end deleteLabTypes function
 
-    function deleteLabTypes($delTestTypeId) {
+    function deleteLabTypes($delTestTypeId)
+    {
         try {
 
-            $imgdelete = $this->deleteFile($delTestTypeId, 'id' , 'image', 'tests_types', LABTEST_IMG_DIR);
+            $imgdelete = $this->deleteFile($delTestTypeId, 'id', 'image', 'tests_types', LABTEST_IMG_DIR);
             if ($imgdelete) {
                 $deletelabType = "DELETE FROM `tests_types` WHERE `id` = ?";
-                
+
                 $stmt = $this->conn->prepare($deletelabType);
-                
+
                 if ($stmt === false) {
                     throw new Exception('Statement preparation failed: ' . $this->conn->error);
                 }
-                
+
                 $stmt->bind_param("i", $delTestTypeId);
-                
+
                 $stmt->execute();
-                
+
                 if ($stmt->error) {
                     throw new Exception('Statement execution failed: ' . $stmt->error);
                 }
-                
-                if($stmt->affected_rows > 0){
-                    return json_encode(['status'=> true, 'message'=> 'success']);
-                }else {
-                    return json_encode(['status'=> false, 'message'=> 'Image Deleted But Details Not Deleted!']);
+
+                if ($stmt->affected_rows > 0) {
+                    return json_encode(['status' => true, 'message' => 'success']);
+                } else {
+                    return json_encode(['status' => false, 'message' => 'Image Deleted But Details Not Deleted!']);
                 }
-                
+
                 $stmt->close();
-            }else {
-                return json_encode(['status'=> false, 'message'=> 'Image Not Deleted!']);
+            } else {
+                return json_encode(['status' => false, 'message' => 'Image Not Deleted!']);
             }
         } catch (Exception $e) {
             error_log($e->getMessage()); // Log the error message
             return false; // Return false in case of error
         }
     } // end deleteLabTypes function
-    
+
 
 } //end of LabTypes Class
