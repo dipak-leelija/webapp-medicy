3b056b2c3df5e1d4262786c9dafac789019c7399
ROODRO <roodro.leelija@gmail.com>
2024-06-05 18:30:47 +0530

    modify code as needed.

--- reports.php
+++ reports.php
@@ -1,9 +1,9 @@
 <?php
-    require_once __DIR__ . '/config/constant.php';
-    require_once ROOT_DIR . '_config/sessionCheck.php'; //check admin loggedin or not
-    
-    require_once CLASS_DIR . 'dbconnect.php';
-    require_once ROOT_DIR . '_config/healthcare.inc.php';
+require_once __DIR__ . '/config/constant.php';
+require_once ROOT_DIR . '_config/sessionCheck.php'; //check admin loggedin or not
+
+require_once CLASS_DIR . 'dbconnect.php';
+require_once ROOT_DIR . '_config/healthcare.inc.php';
 ?>
 
 <!DOCTYPE html>
@@ -24,10 +24,11 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <link href="<?php echo CSS_PATH; ?>sb-admin-2.css" rel="stylesheet">
+    <link href="<?php echo CSS_PATH; ?>bootstrap 5/bootstrap.css" rel="stylesheet">
     <link href="<?php echo CSS_PATH; ?>custom/custom.css" rel="stylesheet">
 
     <script src="<?php echo JS_PATH; ?>ajax.custom-lib.js"></script>
-    
+
 </head>
 
 <body id="page-top">
@@ -36,7 +37,9 @@
     <div id="wrapper">
 
         <!-- sidebar -->
-        <?php include ROOT_COMPONENT.'sidebar.php'; ?>
+        <?php
+        // include ROOT_COMPONENT.'sidebar.php'; 
+        ?>
         <!-- end sidebar -->
 
         <!-- Content Wrapper -->
@@ -46,15 +49,55 @@
             <div id="content">
 
                 <!-- Topbar -->
-                <?php include ROOT_COMPONENT.'topbar.php'; ?>
+                <?php include ROOT_COMPONENT . 'report-topbar.php'; ?>
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="shadow d-flex" style="min-height: 70vh;">
+                                <div class="col-3">
+                                    <div class="card-body">
+                                        <a href="purchase-report.php">
+                                            <button type="button" id="add-testType" class="btn btn-light w-100 border rounded text-center py-2 mb-3 mt-3">
+                                                Purchae Report
+                                            </button>
+                                        </a>
 
+                                        <button type="button" id="add-subTest" class="btn btn-light w-100 border rounded text-center py-2">
+                                            Sales Report
+                                        </button>
+                                    </div>
+                                </div>
+                                <div class="vr mx-2"></div>
+                                <div class="col-8">
+                                    <div class="col-md-12 mt-3 d-flex p-3">
+                                        <div class="col-sm-2 p-2 shadow m-1 text-center">
+                                            blcok 1
+                                        </div>
+                                        <div class="col-sm-2 p-2 shadow m-1 text-center">
+                                            blcok 2
+                                        </div>
+                                        <div class="col-sm-2 p-2 shadow m-1 text-center">
+                                            blcok 3
+                                        </div>
+                                        <div class="col-sm-2 p-2 shadow m-1 text-center">
+                                            blcok 4
+                                        </div>
+                                        <div class="col-sm-2 p-2 shadow m-1 text-center">
+                                            blcok 5
+                                        </div>
+                                        <div class="col-sm-2 p-2 shadow m-1 text-center">
+                                            blcok 6
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <!-- /.container-fluid -->
 
