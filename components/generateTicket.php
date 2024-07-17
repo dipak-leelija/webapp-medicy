9c6dd4a39088c1ada6a369745b50720d70b1b8db
Jayanta <jbleelija@gmail.com>
2024-07-17 14:16:07 +0530

    add new page for ticket generate icon

---
+++ components/generateTicket.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo CSS_PATH; ?>generateTicket.css">
+</head>
+<body>
+    <a href="" type="button" class="btn btn-lg g-ticketBtn rounded-circle text-white" data-toggle="tooltip" data-placement="top" title="Generate Ticket">
+        ?
+    </a>
+</body>
+</html>
