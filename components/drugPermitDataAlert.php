42714e43aa4a0faaa563db5b92ab7404cffee41e
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2024-07-24 11:23:56 +0530

    Drug Permit Alert HTML Updated

--- components/drugPermitDataAlert.php
+++ components/drugPermitDataAlert.php
@@ -1,10 +1,14 @@
 <?php if (empty($form20Data) || empty($form21Data) || (empty($gstinData) && empty($panData))) : ?>
-    <div class="col-12 d-flex justify-content-end alert alert-warning fade show" role="alert" id="drugPermitAlertDiv">
-        <div class="col-md-11" id="drugPermitAlertMsgDiv">
-            <strong>Please Upload Drug Permit Documents!</strong>
-        </div>
-        <div class="col-md-1 d-flex justify-content-end">
-            <a href="<?= URL ?>clinic-setting.php?tab-control" class="btn btn-sm btn-primary"><i class="fas fa-upload"></i></a>
+    <div class="row" style="z-index: 999;">
+        <div class="col-12">
+            <div class="col-12 d-flex justify-content-end alert alert-warning fade show" role="alert" id="drugPermitAlertDiv">
+                <div class="col-md-11" id="drugPermitAlertMsgDiv">
+                    <strong>Please Upload Drug Permit Documents!</strong>
+                </div>
+                <div class="col-md-1 d-flex justify-content-end">
+                    <a href="<?= URL ?>clinic-setting.php?tab-control" class="btn btn-sm btn-primary"><i class="fas fa-upload"></i></a>
+                </div>
+            </div>
         </div>
     </div>
 <?php endif; ?>
