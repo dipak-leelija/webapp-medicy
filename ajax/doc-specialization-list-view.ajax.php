d96929056d8b64679ecc8e7d419e8f0484079cb6
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2024-07-31 18:18:21 +0530

    Doctor Insertion Debugging

--- ajax/doc-specialization-list-view.ajax.php
+++ ajax/doc-specialization-list-view.ajax.php
@@ -12,7 +12,7 @@ if (isset($_GET['match'])) {
     $match = htmlspecialchars($_GET['match']);
 
     $searchResult = json_decode($DoctorCategory->showDoctorCategoryByLikeWise($match));
-    // print_r($searchResult);
+    print_r($searchResult);
 
     if ($searchResult->status) {
         $data = $searchResult->data;
